4dbf97f1c2c515311271ee7ee7ff9d0bead595d5
drcjt <drcjt@outlook.com>
2021-05-19 19:02:15 +0100

    Initial commit

---
+++ ILCompiler/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Collections.Generic;
+
+namespace ILCompiler
+{
+    class Program
+    {
+        private string _outputFilePath;
+        private string _inputFilePath;
+
+        static int Main(string[] args)
+        {
+            try
+            {
+                return new Program().Run(args);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Error: " + e.Message);
+                Console.Error.WriteLine(e.ToString());
+                return 1;
+            }
+        }
+
+        private int Run(string[] args)
+        {
+            return ParseCommandLine(args);
+        }
+
+        private int ParseCommandLine(string[] args)
+        {
+            var rootCommand = new RootCommand
+            {
+                new Argument<string>("Input file to compile"),
+                new Option<string>(new[] { "-o", "--out" }, "Output file path") { Required = true },
+            };
+
+            rootCommand.Description = "CSharp-80 compiler from C# IL to Z80 for TRS-80 Machines";
+            rootCommand.Handler = CommandHandler.Create<string, string>(HandleCommand);
+
+            return rootCommand.Invoke(args);
+        }
+
+        private void HandleCommand(string inputFilePath, string outputFilePath)
+        {
+            _inputFilePath = inputFilePath;
+            _outputFilePath = outputFilePath;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CSharp-80
 C# AOT compiler to Z80 for TRS-80 machines
+
+Idea is to support minimal subset of C# with tiny runtime system and target Z80 machine code to run on TRS-80 model 1
+
+Would be great to get a version of snake up and running not to dissimilar to how it has been done here -> https://medium.com/@MStrehovsky/building-a-self-contained-game-in-c-under-8-kilobytes-74c3cf60ea04
