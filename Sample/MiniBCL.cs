6142edf93bea457cc07e5fc5ba917f171f034801
drcjt <drcjt@outlookc.om>
2021-06-14 22:35:38 +0100

    Separate Pal from BCL

--- Sample/MiniBCL.cs
+++ Sample/MiniBCL.cs
@@ -32,26 +32,6 @@
 	public abstract class ValueType { }
 	public abstract class Enum : ValueType { }
 	public class Attribute { }
-
-	public class Console
-	{
-
-		[System.Runtime.CompilerServices.Intrinsic]
-		public static void Write(char value) { }
-		[System.Runtime.CompilerServices.Intrinsic]
-		public static void Write(int value) { }
-		[System.Runtime.CompilerServices.Intrinsic]
-		public static void Write(string str) { }
-
-		// Test of using dll import to link to native assembly
-		// TODO: Consider if this is better/worse than either intrinsics or
-		// perhaps using MethodImplOptions.InternalCall
-		[System.Runtime.InteropServices.DllImport("Runtime", EntryPoint = "WRITE")]
-		public static unsafe extern bool WriteCh(char ch);
-
-
-		public static void WriteLine(string str) { Write(str); Write('\n'); }
-	}
 }
 
 namespace System.Runtime.CompilerServices
