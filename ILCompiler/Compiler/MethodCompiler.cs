71eeda17dca4b525a13f1040de269e84ba9cd16d
drcjt <drcjt@outlookc.om>
2021-06-23 21:15:25 +0100

    Minor tidyup Map types that can fit into Int16

--- ILCompiler/Compiler/MethodCompiler.cs
+++ ILCompiler/Compiler/MethodCompiler.cs
@@ -4,11 +4,6 @@ using ILCompiler.Common.TypeSystem.IL;
 using ILCompiler.Compiler.DependencyAnalysis;
 using ILCompiler.Interfaces;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ILCompiler.Compiler
 {
