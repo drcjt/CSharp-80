c4e724847457bc45d7947f9c68321542cc44550e
drcjt <drcjt@outlookc.om>
2021-07-11 20:34:20 +0100

    Add Snake sample so can start to get real SeeSharpSnake code compiling. Will start with Random number generator Fix post build steps for building Sample.Dsk to be less specific to build configuration.

---
+++ Samples/Snake/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MiniBCL
+{
+    public static class Program
+    {
+        public static int Main()
+		{
+            Console.Clear();
+
+            uint val = Next(0);
+            Console.WriteLine(val);
+            val = Next(val);
+            Console.WriteLine(val);
+            val = Next(val);
+            Console.WriteLine(val);
+            val = Next(val);
+            Console.WriteLine(val);
+            val = Next(val);
+            Console.WriteLine(val);
+
+            return 0;
+        }
+
+        // TODO: Needs Rem.un to be implemented
+        public static uint Next(uint val)
+        {
+            uint next = (1103515245 * val + 12345) % 2147483648;
+            return next;
+        }
+    }
+}
