317435b3feb1bd237e0cc1f72e51488841555720
Colin Taylor <drcjt@hotmail.com>
2021-08-08 15:07:45 +0100

    Fixed path to ilasm

--- Tests/ILCompiler.IntegrationTests/build_il.bat
+++ Tests/ILCompiler.IntegrationTests/build_il.bat
@@ -13,7 +13,7 @@ mkdir %outdir%\il_bvt
 robocopy .\il_bvt %outdir%\il_bvt
 
 for /f "tokens=* delims= " %%F in ('dir /b .\il_bvt\*.il') do (
-%USERPROFILE%\.nuget\packages\microsoft.netcore.ilasm\5.0.0\runtime\native\ilasm.exe .\il_bvt\%%F
+%USERPROFILE%\.nuget\packages\microsoft.netcore.ilasm\5.0.0\runtimes\native\ilasm.exe .\il_bvt\%%F
 %solutiondir%\ILCompiler\%outdir%\ILCompiler.exe --ignoreUnknownCil --corelibPath %solutiondir%\cs80corlib\bin\Debug\net5.0\cs80corlib.dll --outputFile %outdir%\il_bvt\%%~nF.asm .\il_bvt\%%~nF.exe
 %solutiondir%\tools\zmac.exe --oo cim -o %outdir%\il_bvt\%%~nF.cim %outdir%\il_bvt\%%~nF.asm
 )
