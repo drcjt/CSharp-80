21e2b2d0e15b685e36faf1db8a92ec4ca7a9b88f
Colin Taylor <drcjt@hotmail.com>
2021-10-01 23:05:07 +0100

    Add dot net bot sample

---
+++ Samples/NetBot/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Drawing;
+
+namespace MiniBCL
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.Clear();
+            DrawDotNetBot();
+        }
+
+        private static void DrawDotNetBot()
+        {
+            Graphics.SetPixel(12, 1);
+            Graphics.DrawLine(11, 2, 13, 2);
+            Graphics.DrawLine(12, 3, 13, 3);
+            Graphics.DrawLine(12, 4, 13, 4);
+            Graphics.DrawLine(12, 5, 13, 5);
+            Graphics.DrawLine(27, 5, 28, 5);
+            Graphics.DrawLine(10, 6, 17, 6);
+            Graphics.DrawLine(26, 6, 29, 6);
+            Graphics.DrawLine(8, 7, 19, 7);
+            Graphics.DrawLine(24, 7, 29, 7);
+            Graphics.DrawLine(7, 8, 20, 8);
+            Graphics.DrawLine(24, 8, 29, 8);
+            Graphics.DrawLine(6, 9, 21, 9);
+            Graphics.DrawLine(25, 9, 29, 9);
+            Graphics.DrawLine(6, 10, 22, 10);
+            Graphics.DrawLine(26, 10, 28, 10);
+            Graphics.DrawLine(5, 11, 22, 11);
+            Graphics.DrawLine(27, 11, 28, 11);
+            Graphics.DrawLine(5, 12, 23, 12);
+            Graphics.DrawLine(27, 12, 28, 12);
+            Graphics.DrawLine(4, 13, 23, 13);
+            Graphics.SetPixel(27, 13);
+            Graphics.DrawLine(4, 14, 11, 14);
+            Graphics.DrawLine(26, 14, 27, 14);
+            Graphics.DrawLine(4, 15, 11, 15);
+            Graphics.SetPixel(15, 15);
+            Graphics.SetPixel(21, 15);
+            Graphics.DrawLine(25, 15, 26, 15);
+            Graphics.DrawLine(4, 16, 11, 16);
+            Graphics.SetPixel(15, 16);
+            Graphics.SetPixel(21, 16);
+            Graphics.DrawLine(23, 16, 25, 16);
+            Graphics.DrawLine(4, 17, 11, 17);
+            Graphics.DrawLine(18, 17, 20, 17);
+            Graphics.DrawLine(23, 17, 24, 17);
+            Graphics.DrawLine(3, 18, 23, 18);
+            Graphics.DrawLine(2, 19, 22, 19);
+            Graphics.DrawLine(2, 20, 3, 20);
+            Graphics.DrawLine(6, 20, 16, 20);
+            Graphics.DrawLine(21, 20, 22, 20);
+            Graphics.SetPixel(2, 21);
+            Graphics.SetPixel(18, 21);
+            Graphics.SetPixel(21, 21);
+            Graphics.DrawLine(1, 22, 2, 22);
+            Graphics.DrawLine(7, 22, 16, 22);
+            Graphics.SetPixel(21, 22);
+            Graphics.DrawLine(1, 23, 2, 23);
+            Graphics.DrawLine(8, 23, 20, 23);
+            Graphics.DrawLine(2, 24, 5, 24);
+            Graphics.DrawLine(10, 24, 17, 24);
+            Graphics.DrawLine(1, 25, 6, 25);
+            Graphics.DrawLine(11, 25, 12, 25);
+            Graphics.DrawLine(16, 25, 17, 25);
+            Graphics.DrawLine(1, 26, 4, 26);
+            Graphics.SetPixel(11, 26);
+            Graphics.DrawLine(16, 26, 17, 26);
+            Graphics.DrawLine(1, 27, 5, 27);
+            Graphics.DrawLine(10, 27, 11, 27);
+            Graphics.DrawLine(16, 27, 17, 27);
+            Graphics.DrawLine(2, 28, 4, 28);
+            Graphics.DrawLine(10, 28, 11, 28);
+            Graphics.SetPixel(17, 28);
+            Graphics.SetPixel(4, 29);
+            Graphics.DrawLine(10, 29, 11, 29);
+            Graphics.SetPixel(17, 29);
+            Graphics.DrawLine(10, 30, 11, 30);
+            Graphics.DrawLine(16, 30, 18, 30);
+            Graphics.DrawLine(10, 31, 12, 31);
+            Graphics.DrawLine(16, 31, 18, 31);
+            Graphics.DrawLine(10, 32, 12, 32);
+            Graphics.DrawLine(16, 32, 19, 32);
+            Graphics.DrawLine(9, 33, 12, 33);
+            Graphics.DrawLine(16, 33, 20, 33);
+            Graphics.DrawLine(9, 34, 13, 34);
+            Graphics.DrawLine(16, 34, 21, 34);
+            Graphics.DrawLine(9, 35, 13, 35);
+            Graphics.DrawLine(16, 35, 22, 35);
+            Graphics.DrawLine(9, 36, 13, 36);
+            Graphics.DrawLine(16, 36, 22, 36);
+
+            while (true) ;
+        }
+    }
+}
