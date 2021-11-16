1cb0846aac7aa2245ecd78eeb2015b38783ce96f
Colin Taylor <drcjt@hotmail.com>
2021-11-16 21:38:18 +0000

    Collocate IoC setup for code generators and importers with the code

---
+++ ILCompiler/Compiler/CodeGenerators/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using ILCompiler.Compiler.EvaluationStack;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ILCompiler.Compiler.CodeGenerators
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCodeGenerators(this IServiceCollection services)
+        {
+            services.AddSingleton<ICodeGenerator<BinaryOperator>, BinaryOperatorCodeGenerator>();
+            services.AddSingleton<ICodeGenerator<CallEntry>, CallCodeGenerator>();
+            services.AddSingleton<ICodeGenerator<CastEntry>, CastCodeGenerator>();
+            services.AddSingleton<ICodeGenerator<FieldAddressEntry>, FieldAddressCodeGenerator>();
+            services.AddSingleton<ICodeGenerator<Int32ConstantEntry>, Int32ConstantCodeGenerator>();
+            services.AddSingleton<ICodeGenerator<IntrinsicEntry>, IntrinsicCodeGenerator>();
+            services.AddSingleton<ICodeGenerator<JumpEntry>, JumpCodeGenerator>();
+            services.AddSingleton<ICodeGenerator<JumpTrueEntry>, JumpTrueCodeGenerator>();
+            services.AddSingleton<ICodeGenerator<LocalVariableAddressEntry>, LocalVariableAddressCodeGenerator>();
+            services.AddSingleton<ICodeGenerator<StringConstantEntry>, StringConstantCodeGenerator>();
+            services.AddSingleton<ICodeGenerator<SwitchEntry>, SwitchCodeGenerator>();
+            services.AddSingleton<ICodeGenerator<UnaryOperator>, UnaryOperatorCodeGenerator>();
+            services.AddSingleton<ICodeGenerator<StoreIndEntry>, StoreIndCodeGenerator>();
+            services.AddSingleton<ICodeGenerator<IndirectEntry>, IndirectCodeGenerator>();
+            services.AddSingleton<ICodeGenerator<LocalVariableEntry>, LocalVariableCodeGenerator>();
+            services.AddSingleton<ICodeGenerator<StoreLocalVariableEntry>, StoreLocalVariableCodeGenerator>();
+            services.AddSingleton<ICodeGenerator<ReturnEntry>, ReturnCodeGenerator>();
+
+            return services;
+        }
+    }
+}
--- ILCompiler/IoC/ServiceProviderFactory.cs
+++ ILCompiler/IoC/ServiceProviderFactory.cs
@@ -18,19 +18,18 @@ namespace ILCompiler.IoC
         {
             var services = new ServiceCollection();
             ConfigureServices(services);
-            ConfigureImporters(services);
-
-            services.AddSingleton<CodeGeneratorFactory>();
-            ConfigureCodeGenerators(services);
 
             services.AddFactory<IMethodCompiler>();
             services.AddTransient<IMethodCompiler, MethodCompiler>();
 
+            services.AddSingleton<CodeGeneratorFactory>();
             services.AddFactory<ICodeGenerator>();
             services.AddTransient<ICodeGenerator, CodeGenerator>();
+            services.AddCodeGenerators();
 
             services.AddFactory<IILImporter>();
             services.AddTransient<IILImporter, ILImporter>();
+            services.AddImporters();
 
             services.AddSingleton<Z80Writer>();
 
@@ -47,58 +46,7 @@ namespace ILCompiler.IoC
             services.AddSingleton<INameMangler, NameMangler>();
 
             services.AddSingleton<IOpcodeImporterFactory, OpcodeImporterFactory>();
-            services.AddSingleton<ICodeGeneratorFactory, Compiler.CodeGenerators.CodeGeneratorFactory>();
-        }
-
-        private static void ConfigureCodeGenerators(ServiceCollection services)
-        {
-            services.AddSingleton<ICodeGenerator<BinaryOperator>, BinaryOperatorCodeGenerator>();
-            services.AddSingleton<ICodeGenerator<CallEntry>, CallCodeGenerator>();
-            services.AddSingleton<ICodeGenerator<CastEntry>, CastCodeGenerator>();
-            services.AddSingleton<ICodeGenerator<FieldAddressEntry>, FieldAddressCodeGenerator>();
-            services.AddSingleton<ICodeGenerator<Int32ConstantEntry>, Int32ConstantCodeGenerator>();
-            services.AddSingleton<ICodeGenerator<IntrinsicEntry>, IntrinsicCodeGenerator>();
-            services.AddSingleton<ICodeGenerator<JumpEntry>, JumpCodeGenerator>();
-            services.AddSingleton<ICodeGenerator<JumpTrueEntry>, JumpTrueCodeGenerator>();
-            services.AddSingleton<ICodeGenerator<LocalVariableAddressEntry>, LocalVariableAddressCodeGenerator>();
-            services.AddSingleton<ICodeGenerator<StringConstantEntry>, StringConstantCodeGenerator>();
-            services.AddSingleton<ICodeGenerator<SwitchEntry>, SwitchCodeGenerator>();
-            services.AddSingleton<ICodeGenerator<UnaryOperator>, UnaryOperatorCodeGenerator>();
-            services.AddSingleton<ICodeGenerator<StoreIndEntry>, StoreIndCodeGenerator>();
-            services.AddSingleton<ICodeGenerator<IndirectEntry>, IndirectCodeGenerator>();
-            services.AddSingleton<ICodeGenerator<LocalVariableEntry>, LocalVariableCodeGenerator>();
-            services.AddSingleton<ICodeGenerator<StoreLocalVariableEntry>, StoreLocalVariableCodeGenerator>();
-            services.AddSingleton<ICodeGenerator<ReturnEntry>, ReturnCodeGenerator>();
-        }
-
-        private static void ConfigureImporters(ServiceCollection services)
-        {
-            services.AddSingleton<IOpcodeImporter, NopImporter>();
-            services.AddSingleton<IOpcodeImporter, LoadIntImporter>();
-            services.AddSingleton<IOpcodeImporter, StoreVarImporter>();
-            services.AddSingleton<IOpcodeImporter, LoadVarImporter>();
-            services.AddSingleton<IOpcodeImporter, AddressOfVarImporter>();
-            services.AddSingleton<IOpcodeImporter, StoreIndirectImporter>();
-            services.AddSingleton<IOpcodeImporter, LoadIndirectImporter>();
-            services.AddSingleton<IOpcodeImporter, StoreFieldImporter>();
-            services.AddSingleton<IOpcodeImporter, LoadFieldImporter>();
-            services.AddSingleton<IOpcodeImporter, BinaryOperationImporter>();
-            services.AddSingleton<IOpcodeImporter, CompareImporter>();
-            services.AddSingleton<IOpcodeImporter, BranchImporter>();
-            services.AddSingleton<IOpcodeImporter, LoadArgImporter>();
-            services.AddSingleton<IOpcodeImporter, StoreArgImporter>();
-            services.AddSingleton<IOpcodeImporter, LoadStringImporter>();
-            services.AddSingleton<IOpcodeImporter, InitobjImporter>();
-            services.AddSingleton<IOpcodeImporter, ConversionImporter>();
-            services.AddSingleton<IOpcodeImporter, NegImporter>();
-            services.AddSingleton<IOpcodeImporter, RetImporter>();
-            services.AddSingleton<IOpcodeImporter, CallImporter>();
-            services.AddSingleton<IOpcodeImporter, DupImporter>();
-            services.AddSingleton<IOpcodeImporter, NewobjImporter>();
-            services.AddSingleton<IOpcodeImporter, PopImporter>();
-            services.AddSingleton<IOpcodeImporter, AddressOfFieldImporter>();
-            services.AddSingleton<IOpcodeImporter, SwitchImporter>();
-            services.AddSingleton<IOpcodeImporter, LocallocImporter>();
+            services.AddSingleton<ICodeGeneratorFactory, CodeGeneratorFactory>();
         }
     }
 }
