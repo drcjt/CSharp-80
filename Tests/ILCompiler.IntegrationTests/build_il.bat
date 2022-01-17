cab940cf766f9446e51fd88e60672ed25f4c8996
Colin Taylor <drcjt@hotmail.com>
2022-01-17 19:35:02 +0000

    Fix integration tests to work correctly in any configuration e.g. debug and release

--- Tests/ILCompiler.IntegrationTests/build_il.bat
+++ Tests/ILCompiler.IntegrationTests/build_il.bat
@@ -6,6 +6,7 @@ REM Runs zmac on the asm's produced by the ilcompiler
 set solutiondir=%1
 set outdir=%2
 set targetdir=%3
+set configuration=%4
 
 call %1\tools\get-tools.cmd
 
@@ -15,6 +16,6 @@ robocopy /njh /njs /ns /nc /ndl /np .\il_bvt %outdir%\il_bvt *.il
 for /f "tokens=* delims= " %%F in ('dir /b .\il_bvt\*.il') do (
 @echo Preparing Bvt Il: %%F
 %USERPROFILE%\.nuget\packages\microsoft.netcore.ilasm\5.0.0\runtimes\native\ilasm.exe /quiet .\il_bvt\%%F
-%solutiondir%\ILCompiler\%outdir%\ILCompiler.exe --ignoreUnknownCil false --printReturnCode false --integrationTests true --corelibPath %solutiondir%\System.Private.CoreLib\bin\Release\net6.0\System.Private.CoreLib.dll --outputFile %outdir%\il_bvt\%%~nF.asm .\il_bvt\%%~nF.exe
+%solutiondir%\ILCompiler\%outdir%\ILCompiler.exe --ignoreUnknownCil false --printReturnCode false --integrationTests true --corelibPath %solutiondir%\System.Private.CoreLib\bin\%configuration%\net6.0\System.Private.CoreLib.dll --outputFile %outdir%\il_bvt\%%~nF.asm .\il_bvt\%%~nF.exe
 %solutiondir%\tools\zmac.exe --oo cim -o %outdir%\il_bvt\%%~nF.cim %outdir%\il_bvt\%%~nF.asm
 )
