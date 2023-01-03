e8ff57523448193c2e879d525a4dcd04dd5a6744
Colin Taylor <drcjt@hotmail.com>
2023-01-03 20:12:51 +0000

    Start to fill in some CoreLib methods (#86)
    Fix bug in copy helper when offset is < -128 offset
    ILImporter needs to do better comparison of types when falling through from basic block with things on the stack.
    Add tests for CoreLib
    
    Co-authored-by: Colin Taylor <drcjt@outlook.com>

---
+++ Tests/CoreLib/CoreLib/CoreLib.cs
@@ -0,0 +1,14 @@
+﻿namespace CoreLib
+{
+    public static class CoreLib
+    {
+        public static int Main()
+        {
+            var result = true;
+            result = result && CharTests.IsBetweenCharTests();
+            result = result && CharTests.IsAsciiDigit_WithAsciiDigits_ReturnsTrue();
+            result = result && CharTests.IsAsciiDigit_WithNonAsciiDigits_ReturnsFalse();
+            return result ? 0 : 1;
+        }
+    }
+}
