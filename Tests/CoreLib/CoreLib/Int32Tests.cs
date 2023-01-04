8d65a74f06c09b973a8d3416ae06c0afafa25ed0
Colin Taylor <drcjt@outlook.com>
2023-01-04 20:36:25 +0000

    Add simple Int32.Parse method with tests Fix bug in CopyHelper Detail ECMA 335 section for merging stack states in ImportFallThrough

---
+++ Tests/CoreLib/CoreLib/Int32Tests.cs
@@ -0,0 +1,24 @@
+﻿namespace CoreLib
+{
+    public class Int32Tests
+    {
+        public static bool Parse_Valid()
+        {
+            var result = true;
+
+            result = result && AssertEqual(0, int.Parse("0"));
+            result = result && AssertEqual(0, int.Parse("0000000000000000000000000000000000000000000000000000000000"));
+            result = result && AssertEqual(1, int.Parse("0000000000000000000000000000000000000000000000000000000001"));
+            result = result && AssertEqual(2147483647, int.Parse("2147483647"));
+            result = result && AssertEqual(2147483647, int.Parse("02147483647"));
+            result = result && AssertEqual(2147483647, int.Parse("00000000000000000000000000000000000000000000000002147483647"));
+            result = result && AssertEqual(123, int.Parse("123\0\0"));
+
+            result = result && AssertEqual(123, int.Parse("123"));
+
+            return result;
+        }
+
+        private static bool AssertEqual(int expected, int actual) => expected == actual;
+    }
+}
