1f26fd9e7c357d9f681829e4bb514af4ad676b17
Colin Taylor <drcjt@outlook.com>
2023-01-05 20:23:04 +0000

    Morph children of PutArgType nodes.

---
+++ Tests/Regression/Regression/RegressionTests.cs
@@ -0,0 +1,24 @@
+﻿public static class Regression
+{
+    public static int Main()
+    {
+        var result = 0;
+
+        result = Bug87();
+        if (result != 0) return 87;
+
+        return 0;
+    }
+
+    public static int Bug87()
+    {
+        char[] test = new char[1] { 'a' };
+        Bug87_Method(test[0]);
+
+        return 0;
+    }
+
+    private static void Bug87_Method(char ch)
+    {
+    }
+}
