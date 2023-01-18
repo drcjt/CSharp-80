a652c91c0ea81868599abb61bdf1f47ab82d89e2
Colin Taylor <drcjt@outlook.com>
2023-01-18 21:17:08 +0000

    Change Pens class to use statics to avoid creating many Pen objects

--- System.Private.CoreLib/System/Drawing/Pens.cs
+++ System.Private.CoreLib/System/Drawing/Pens.cs
@@ -2,12 +2,29 @@
 {
     public sealed class Pens
     {
+        private static Pen s_white = null;
         public static Pen White
         {
             get
             {
-                // TODO: change this when static fields are implemented properly
-                return new Pen(Color.White);
+                if (s_white == null)
+                {
+                    s_white = new Pen(Color.White);
+                }
+                return s_white;
+            }
+        }
+
+        private static Pen s_black = null;
+        public static Pen Black
+        {
+            get
+            {
+                if (s_black == null)
+                {
+                    s_black = new Pen(Color.Black);
+                }
+                return s_black;
             }
         }
     }
