c9ab28af47dc68132ee886b4cb9141defa041386
Colin Taylor <drcjt@hotmail.com>
2023-01-18 22:02:57 +0000

    Improve stind_ref test (#94)
    * Improve stind_ref test
    
    * Use github as source for zmac.exe - hopefully this will eliminate the recent intermittent build errors
    
    Co-authored-by: Colin Taylor <drcjt@outlook.com>

--- tools/get-tools.cmd
+++ tools/get-tools.cmd
@@ -9,7 +9,7 @@ goto end
 
 :download
 
-curl --output zmac.zip http://48k.ca/zmac.zip
+curl -L --output zmac.exe https://github.com/gp48k/zmac/raw/master/zmac.exe
 curl --output trsread.zip http://www.trs-80emulators.com/trsread-4.35.zip
 tar -xf zmac.zip
 tar -xf trsread.zip
