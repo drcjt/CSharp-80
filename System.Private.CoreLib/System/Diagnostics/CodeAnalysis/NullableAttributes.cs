ecf7266fca6055f1639056c7bd82454f5b2a2efe
Colin Taylor <drcjt@hotmail.com>
2023-01-31 21:45:20 +0000

    Fix nullable warnings in Wumpus (#114)
    Required adding some attributes.
    
    Co-authored-by: Colin Taylor <drcjt@outlook.com>

---
+++ System.Private.CoreLib/System/Diagnostics/CodeAnalysis/NullableAttributes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace System.Diagnostics.CodeAnalysis
+{
+    public sealed class MemberNotNullAttribute : Attribute 
+    {
+        public MemberNotNullAttribute(string member) => Members = new[] { member };
+
+        public MemberNotNullAttribute(params string[] members) => Members = members;
+
+        public string[] Members { get; }
+    }
+}
