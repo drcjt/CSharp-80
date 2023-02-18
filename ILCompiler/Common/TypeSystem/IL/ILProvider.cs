d79b01c44587b01803c63caac40ff61b5d8eb9d4
Colin Taylor <drcjt@outlook.com>
2023-02-18 11:57:51 +0000

    Unsafe intrinsics implementation via IL replacement

---
+++ ILCompiler/Common/TypeSystem/IL/ILProvider.cs
@@ -0,0 +1,10 @@
+﻿using dnlib.DotNet.Emit;
+using ILCompiler.Common.TypeSystem.Common;
+
+namespace ILCompiler.Common.TypeSystem.IL
+{
+    public abstract class ILProvider
+    {
+        public abstract CilBody? GetMethodIL(MethodDesc method);
+    }
+}
---
+++ ILCompiler/Common/TypeSystem/IL/UnsafeIntrinsics.cs
@@ -0,0 +1,84 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using ILCompiler.Common.TypeSystem.Common;
+
+namespace ILCompiler.Common.TypeSystem.IL
+{
+    public static class UnsafeIntrinsics
+    {
+        public static CilBody? EmitIL(MethodDesc method)
+        {
+            switch (method.Name)
+            {
+                case "AsPointer":
+                    return EmitAsPointer(method);
+                case "SizeOf":
+                    return EmitSizeOf(method);
+                case "Add":
+                    return EmitAdd(method);
+                case "AddByteOffset":
+                    return EmitAddByteOffset(method);
+            }
+
+            return null;
+        }
+
+        private static CilBody? EmitAsPointer(MethodDesc method)
+        {
+            var body = new CilBody();
+
+            body.Instructions.Add(OpCodes.Ldarg_0.ToInstruction());
+            body.Instructions.Add(OpCodes.Conv_U.ToInstruction());
+            body.Instructions.Add(OpCodes.Ret.ToInstruction());
+
+            body.UpdateInstructionOffsets();
+
+            return body;
+        }
+
+        private static CilBody? EmitSizeOf(MethodDesc method) 
+        {
+            var body = new CilBody();
+
+            body.Instructions.Add(OpCodes.Ldc_I4_0.ToInstruction()); // Replace with code below when Sizeof supported
+            //body.Instructions.Add(OpCodes.Sizeof.ToInstruction(method.ResolveType(new GenericMVar(0)).ToTypeDefOrRef()));
+            body.Instructions.Add(OpCodes.Ret.ToInstruction());
+
+            body.UpdateInstructionOffsets();
+
+            return body;            
+        }
+
+        private static CilBody? EmitAdd(MethodDesc method)
+        {
+            var body = new CilBody();
+
+            body.Instructions.Add(OpCodes.Ldarg_1.ToInstruction());
+            body.Instructions.Add(OpCodes.Ldc_I4_0.ToInstruction()); // Replace with code below when Sizeof supported
+            //body.Instructions.Add(OpCodes.Sizeof.ToInstruction(method.ResolveType(new GenericMVar(0)).ToTypeDefOrRef()));
+            body.Instructions.Add(OpCodes.Conv_I.ToInstruction());
+            body.Instructions.Add(OpCodes.Mul.ToInstruction());
+            body.Instructions.Add(OpCodes.Ldarg_0.ToInstruction());
+            body.Instructions.Add(OpCodes.Add.ToInstruction());
+            body.Instructions.Add(OpCodes.Ret.ToInstruction());
+
+            body.UpdateInstructionOffsets();
+
+            return body;
+        }
+
+        private static CilBody? EmitAddByteOffset(MethodDesc method) 
+        {
+            var body = new CilBody();
+
+            body.Instructions.Add(OpCodes.Ldarg_0.ToInstruction());
+            body.Instructions.Add(OpCodes.Ldarg_1.ToInstruction());
+            body.Instructions.Add(OpCodes.Add.ToInstruction(method.ResolveType(new GenericMVar(0)).ToTypeDefOrRef()));
+            body.Instructions.Add(OpCodes.Ret.ToInstruction());
+
+            body.UpdateInstructionOffsets();
+
+            return body;
+        }
+    }
+}
