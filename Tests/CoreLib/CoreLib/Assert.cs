683a25074607319d0a5ad0b5add560cb5684d434
Colin Taylor <drcjt@hotmail.com>
2023-02-23 20:48:30 +0000

    Improve tests by implementing Assert.Equals using Environment.Exit (#145)
    Co-authored-by: Colin Taylor <drcjt@outlook.com>

---
+++ Tests/CoreLib/CoreLib/Assert.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CoreLib
+{
+    internal static class Assert
+    {
+        public static void Equals(bool expected, bool actual)
+        {
+            if (expected != actual)
+            {
+                Environment.Exit(1);
+            }
+        }
+
+        public static void Equals(int expected, int actual)
+        {
+            if (expected != actual)
+            {
+                Environment.Exit(1);
+            }
+        }
+    }
+}
