3bb9f658134263c7e7b207a94fd5b9cfa7a36b20
Colin Taylor <drcjt@outlook.com>
2023-03-25 13:25:35 +0000

    Simplify build_il.bat as no longer needs assemble the il, run the il compiler, and run the z80 assembler. Hopefully in the future this batch script used in the build events can be removed completely.

--- Tests/ILCompiler.IntegrationTests/build_il.bat
+++ Tests/ILCompiler.IntegrationTests/build_il.bat
@@ -1,21 +1,9 @@
-REM Creates input files for the nunit tests
-REM Runs ilasm on the il_bvt il files
-REM Runs ilcompiler on the exe's produced by ilasm
-REM Runs zmac on the asm's produced by the ilcompiler
+REM Ensures the zmac assembler has been downloaded
+REM copies test il files to appropriate location for integration tests to run
 
-set solutiondir=%1
 set outdir=%2
-set targetdir=%3
-set configuration=%4
 
 call %1\tools\get-tools.cmd
 
 mkdir %outdir%\il_bvt
 robocopy /njh /njs /ns /nc /ndl /np .\il_bvt %outdir%\il_bvt *.il
-
-for /f "tokens=* delims= " %%F in ('dir /b .\il_bvt\*.il') do (
-@echo Preparing Bvt Il: %%F
-%USERPROFILE%\.nuget\packages\microsoft.netcore.ilasm\6.0.0\runtimes\native\ilasm.exe /quiet .\il_bvt\%%F
-%solutiondir%\ILCompiler\%outdir%\ILCompiler.exe --ignoreUnknownCil false --printReturnCode false --integrationTests true --corelibPath %solutiondir%\System.Private.CoreLib\bin\%configuration%\net7.0\System.Private.CoreLib.dll --outputFile %outdir%\il_bvt\%%~nF.asm .\il_bvt\%%~nF.exe
-%solutiondir%\tools\zmac.exe --oo cim -o %outdir%\il_bvt\%%~nF.cim %outdir%\il_bvt\%%~nF.asm
-)
