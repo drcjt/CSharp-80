90f9296d16ce0105f3a6e064192502e8a3013287
Colin Taylor <drcjt@outlook.com>
2023-04-01 21:41:30 +0100

    Integration tests don't need to get tools any more in the build event batch script

--- Tests/ILCompiler.IntegrationTests/build_il.bat
+++ Tests/ILCompiler.IntegrationTests/build_il.bat
@@ -1,9 +1,6 @@
-REM Ensures the zmac assembler has been downloaded
 REM copies test il files to appropriate location for integration tests to run
 
 set outdir=%2
-
-call %1\tools\get-tools.cmd
 
 mkdir %outdir%\il_bvt
 robocopy /njh /njs /ns /nc /ndl /np .\il_bvt %outdir%\il_bvt *.il
