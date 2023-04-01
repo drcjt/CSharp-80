22da9be20a3bfd3fc2aed7b4638a7971fd2f1315
Colin Taylor <drcjt@outlook.com>
2023-04-01 22:34:06 +0100

    Add option to delete assembler before compiling il. Add option to not use cim as output type

--- Tests/ILCompiler.Tests.Common/ILCompilerRunner.cs
+++ Tests/ILCompiler.Tests.Common/ILCompilerRunner.cs
@@ -6,6 +6,7 @@ namespace ILCompiler.Tests.Common
     public class ILCompilerRunner
     {
         public const int StackStart = UInt16.MaxValue;
+        private const string ZmacExe = "zmac.exe";
 
         private readonly string _corelibPath;
 
@@ -23,17 +24,29 @@ namespace ILCompiler.Tests.Common
             return new ILCompilerRunner(solutionPath);
         }
 
-        public void CompileILAndAssemble(string ilFileName, bool createLibrary = true)
+        public void CompileILAndAssemble(string ilFileName, bool createLibrary = true, bool outputCim = true, bool deleteAssembler = false)
         {
-            CompileIL(ilFileName, createLibrary);
+            // If test requires assembler to not already have been downloaded then delete it first
+            if (deleteAssembler)
+            {
+                var ilCompilerApplicationDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ILCompiler");
+                var zmacPath = Path.Combine(ilCompilerApplicationDirectory, ZmacExe);
+                if (File.Exists(zmacPath))
+                {
+                    File.Delete(zmacPath);
+                }
+            }
+
+            CompileIL(ilFileName, createLibrary, outputCim);
         }
 
-        public void CompileIL(string ilFileName, bool createLibrary = true)
+        private void CompileIL(string ilFileName, bool createLibrary, bool outputCim)
         {
             var asmFileName = Path.ChangeExtension(ilFileName, "asm");
             var exeFileName = Path.ChangeExtension(ilFileName, createLibrary ? "dll" : "exe");
 
-            var arguments = $"-ao cim --ignoreUnknownCil false --printReturnCode false --integrationTests true --corelibPath {_corelibPath} --outputFile {asmFileName} {exeFileName} --stackStart {StackStart}";
+            var arguments = outputCim ? "-ao cim " : "";
+            arguments += $"--ignoreUnknownCil false --printReturnCode false --integrationTests true --corelibPath {_corelibPath} --outputFile {asmFileName} {exeFileName} --stackStart {StackStart}";
 
             var compiled = ILCompiler.Program.Main(arguments.Split(' '));
 
