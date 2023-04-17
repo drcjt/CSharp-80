fbf5fa9e722ed90beb47f47fad698a294c3eb52d
Colin Taylor <drcjt@outlook.com>
2023-04-17 21:12:25 +0100

    Add simple memset assembly routine Use memset to implement initblk Add initblk test

---
+++ ILCompiler/Runtime/Memset.asm
@@ -0,0 +1,37 @@
+; This routine allocates a new object with the specified size on top of the stack
+;
+; Uses: HL, DE, BC, AF, BC'
+
+Memset:	
+	EXX
+	POP BC		; Save return address
+	EXX
+
+	POP HL		; Destination address
+
+	POP DE		; Initial value
+	POP AF
+
+	POP BC		; Count
+	POP AF
+
+	EXX
+	PUSH BC		; put return address back
+	EXX
+
+	LD A, B
+	OR C
+
+	RET Z		; Nothing to do if count is zero
+
+	LD A, E
+	LD D, H
+	LD E, L
+
+	LD (HL), A
+	INC DE
+	DEC BC
+
+	LDIR
+
+	RET
