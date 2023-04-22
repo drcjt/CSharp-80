717d6df6210733e3f65eebd067ae7c7a2c4cb58e
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2023-04-22 13:07:53 +0100

    Add primes sieve as directed test for benchmarking (#214)
    Co-authored-by: Colin Taylor <drcjt@outlook.com>

---
+++ Tests/Directed/Primes/Primes.cs
@@ -0,0 +1,52 @@
+﻿namespace Primes
+{
+    public static class Tests
+    {
+        public static int CountPrimesUpto(int bound)
+        {
+            var isPrime = new int[bound];
+            for (int i = 2; i < bound; i++)
+            {
+                isPrime[i] = 1;
+            }
+
+            isPrime[0] = 0;
+            isPrime[1] = 0;
+
+            var thisFactor = 2;
+            while (thisFactor * thisFactor <= bound)
+            {
+                var mark = thisFactor + thisFactor;
+                while (mark <= bound)
+                {
+                    isPrime[mark] = 0;
+                    mark += thisFactor;
+                }
+
+                thisFactor++;
+                while (isPrime[thisFactor] == 0)
+                {
+                    thisFactor++;
+                }
+            }
+
+            var totalPrimes = 0;
+            for (int i = 0; i < bound; i++)
+            {
+                totalPrimes += isPrime[i];
+            }
+
+            return totalPrimes;
+        }
+
+        public static int Main()
+        {
+            if (CountPrimesUpto(1000) != 168)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
