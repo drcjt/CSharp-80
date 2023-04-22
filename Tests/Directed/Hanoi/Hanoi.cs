146ff958136cf4ddfedb62075727448e47583ea4
Colin Taylor <drcjt@outlook.com>
2023-04-22 16:16:18 +0100

    Add towers of hanoi test for benchmarking

---
+++ Tests/Directed/Hanoi/Hanoi.cs
@@ -0,0 +1,33 @@
+﻿namespace Hanoi
+{
+    public static class Tests
+    {
+        private static int numberOfMoves;
+
+        static void MoveTower(int disc, int from, int to, int use)
+        {
+            if (disc > 0)
+            {
+                numberOfMoves++;
+                MoveTower(disc - 1, from, use, to);
+                MoveTower(disc - 1, use, to, from);
+            }
+        }
+
+        public static int Main()
+        {
+            int iterations = 5;
+            int numberOfDiscs = 12;
+
+            numberOfMoves = 0;
+            while (iterations > 0)
+            {
+                iterations--;
+                MoveTower(numberOfDiscs, 1, 3, 2);
+            }
+            if (numberOfMoves != 20475)
+                return 1;
+            return 0;
+        }
+    }
+}
