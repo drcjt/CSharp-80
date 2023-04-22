848d2c87d511dbbb1334f931a3fd42f5bcd2c1f1
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2023-04-22 19:17:36 +0100

    Add towers of hanoi sample (#216)
    Co-authored-by: Colin Taylor <drcjt@outlook.com>

---
+++ Samples/Hanoi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Hanoi
+{
+    public static class Program
+    {
+        private static int NumberOfMoves;
+
+        static void MoveTower(int disc, int from, int to, int use)
+        {
+            if (disc > 0)
+            {
+                NumberOfMoves++;
+                MoveTower(disc - 1, from, use, to);
+                Console.Write("Move disc ");
+                Console.Write(disc);
+                Console.Write(" from tower ");
+                Console.Write(from);
+                Console.Write(" to ");
+                Console.WriteLine(to);
+                MoveTower(disc - 1, use, to, from);
+            }
+        }
+
+        public static void Main()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter number of discs?");
+            var numberOfDiscs = int.Parse(Console.ReadLine());
+
+            MoveTower(numberOfDiscs, 1, 3, 2);
+
+            Console.Write("Number of moves ");
+            Console.WriteLine(NumberOfMoves);
+        }
+    }
+}
