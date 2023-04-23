14de9612283630d0b2c36f5180cafb581e9db50f
Colin Taylor <drcjt@outlook.com>
2023-04-23 23:28:05 +0100

    Add game of life sample using neighbour count cell map format as per Michael Abrash's Graphics Programming Black Book

---
+++ Samples/Life/LehmerRandom.cs
@@ -0,0 +1,32 @@
+﻿namespace Life
+{
+    internal class LehmerRandom
+    {
+        private const int a = 16807;
+        private const int m = 2147483647;
+        private const int q = 127773;
+        private const int r = 2836;
+        private int seed;
+
+        public LehmerRandom(int seed)
+        {
+            this.seed = seed;
+        }
+        public int Next()
+        {
+            int hi = seed / q;
+            int lo = seed % q;
+            seed = (a * lo) - (r * hi);
+            if (seed <= 0)
+            {
+                seed = seed + m;
+            }
+            return seed;
+        }
+
+        public int Next(int value)
+        {
+            return Next() % value;
+        }
+    }
+}
---
+++ Samples/Life/Life.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Life
+{
+    public static class Life
+    {
+        private static CellMap? CurrentMap;
+
+        public static unsafe void Main()
+        {
+            var rows = Console.WindowHeight - 1;
+            var columns = Console.WindowWidth - 1;
+
+            byte* currentCells = stackalloc byte[rows * columns];
+            CurrentMap = new CellMap(rows, columns, currentCells);
+
+            Console.Clear();
+
+            CurrentMap.Init();
+
+            for (var generation = 1; generation < 50; generation++)
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.Write("Generation : ");
+                Console.Write(generation);
+
+                var startTime = Environment.GetDateTime();
+
+                CurrentMap.NextGeneration();
+
+                var endTime = Environment.GetDateTime();
+                var elapsedTime = endTime.TotalSeconds - startTime.TotalSeconds;
+
+                Console.SetCursorPosition(25, 0);
+                Console.Write("Time: ");
+                Console.Write(elapsedTime);
+            }
+        }
+    }
+}
