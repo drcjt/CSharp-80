8b8e2e9ea6e6fee228cf99ea81a4ba590c96a3f3
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2023-07-12 21:08:40 +0100

    Add test to validate that static constructors are called correctly

---
+++ Tests/Methodical/BeforeFieldInit/BeforeFieldInit.cs
@@ -0,0 +1,36 @@
+﻿namespace StaticFieldInit
+{
+    public static class TestClass
+    {
+        public static int retVal = 0;
+        public static int TestMethod(bool cond)
+        {
+            if (cond)
+            {
+                return OtherClass.X;
+            }
+
+            return 0;
+        }
+    }
+
+    public class OtherClass
+    {
+        public static readonly int X = GetX();
+
+        static int GetX()
+        {
+            TestClass.retVal = 1;
+            return 1;
+        }
+    }
+
+    public static class Test
+    {
+        public static int Main()
+        {
+            TestClass.TestMethod(false);
+            return TestClass.retVal;
+        }
+    }
+}
