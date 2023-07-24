4e906bcbb68d0ff7eb00eef2c75d363e730360e0
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2023-07-24 21:04:41 +0100

    Add simple Debug.Assert implementation.

---
+++ System.Private.CoreLib/System/Diagnostics/ConditionalAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace System.Diagnostics
+{
+    internal sealed class ConditionalAttribute : Attribute
+    {
+        public ConditionalAttribute(string conditionString) 
+        {
+            _conditionString = conditionString;
+        }
+
+        private string _conditionString;
+        public string ConditionString
+        {
+            get
+            {
+                return _conditionString;
+            }
+        }
+    }
+}
