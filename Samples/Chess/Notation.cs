b582ae64c85f64a697b4852e2105b9c39a749221
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2023-07-30 12:38:51 +0100

    Initial work on a chess sample.

---
+++ Samples/Chess/Notation.cs
@@ -0,0 +1,64 @@
+﻿namespace Chess
+{
+    public static class Notation
+    {
+        public static char ToChar(Piece piece)
+        {
+            return piece switch
+            {
+                Piece.WhitePawn => 'P',
+                Piece.WhiteKnight => 'N',
+                Piece.WhiteBishop => 'B',
+                Piece.WhiteRook => 'R',
+                Piece.WhiteQueen => 'Q',
+                Piece.WhiteKing => 'K',
+                Piece.BlackPawn => 'p',
+                Piece.BlackKnight => 'n',
+                Piece.BlackBishop => 'b',
+                Piece.BlackRook => 'r',
+                Piece.BlackQueen => 'q',
+                Piece.BlackKing => 'k',
+                _ => '.',
+            };
+        }
+
+        public static Piece ToPiece(char ch)
+        {
+            return ch switch
+            {
+                'P' => Piece.WhitePawn,
+                'N' => Piece.WhiteKnight,
+                'B' => Piece.WhiteBishop,
+                'R' => Piece.WhiteRook,
+                'Q' => Piece.WhiteQueen,
+                'K' => Piece.WhiteKing,
+                'p' => Piece.BlackPawn,
+                'n' => Piece.BlackKnight,
+                'b' => Piece.BlackBishop,
+                'r' => Piece.BlackRook,
+                'q' => Piece.BlackQueen,
+                'k' => Piece.BlackKing,
+                _ => Piece.None,
+                // _ => throw new ArgumentException($"Unknown piece character {ch}");
+            };
+        }
+
+        public static string ToSquareName(byte squareIndex)
+        {
+            int rank = squareIndex / 8;
+            int file = squareIndex % 8;
+
+            char[] squareNotation = new char[2] { (char)('a' + file), (char)('1' + rank) };
+            return new string(squareNotation);
+        }
+
+        public static byte ToSquare(string squareNotation)
+        {
+            int file = squareNotation[0] - 'a';
+            int rank= squareNotation[1] - '1';
+            int index = rank * 8 + file;
+
+            return (byte)index;
+        }
+    }
+}
---
+++ Samples/Chess/Piece.cs
@@ -0,0 +1,51 @@
+﻿namespace Chess
+{
+    public enum Color
+    {
+        Black,
+        White
+    }
+
+    public enum Piece : byte
+    {
+        // 1st bit indicates if piece or not
+        None = 0,
+
+        // 2nd bit indicates color of piece
+        Black = 1,
+        White = 3,
+
+        // 3rd and more bits indicate type of piece
+        Pawn = 4,
+        Knight = 8,
+        Bishop = 12,
+        Rook = 16,
+        Queen = 20,
+        King = 24,
+
+        WhitePawn = White + Pawn,
+        WhiteKnight = White + Knight,
+        WhiteBishop = White + Bishop,
+        WhiteRook = White + Rook,
+        WhiteQueen = White + Queen,
+        WhiteKing = White + King,
+
+        BlackPawn = Black + Pawn,
+        BlackKnight = Black + Knight,
+        BlackBishop = Black + Bishop,
+        BlackRook = Black + Rook,
+        BlackQueen = Black + Queen,
+        BlackKing = Black + King,
+
+        ColorMask = 0b00000011,
+        TypeMask = 0b01111100
+    }
+
+    public static class Pieces
+    {
+        public static Color Color(Piece piece) => (Color)((piece & Piece.ColorMask));
+
+        public static bool IsWhite(Piece piece) => (piece & Piece.ColorMask) == Piece.White;
+        public static bool IsBlack(Piece piece) => (piece & Piece.ColorMask) == Piece.Black;
+    }
+}
