cbb11a40f2d8ad013bc8cd1fed2dfe14476bf872
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2023-08-27 18:26:46 +0100

    Compute dominance frontier Computer iterated dominance frontier Initial code to insert phi functions

--- ILCompiler/Compiler/SsaBuilder.cs
+++ ILCompiler/Compiler/SsaBuilder.cs
@@ -1,6 +1,6 @@
 ﻿using ILCompiler.Compiler.EvaluationStack;
-using ILCompiler.Interfaces;
 using ILCompiler.Compiler.Ssa;
+using ILCompiler.Interfaces;
 using Microsoft.Extensions.Logging;
 using System.Text;
 
@@ -29,11 +29,189 @@ namespace ILCompiler.Compiler
             // Calculate liveness
             LocalVarLiveness(blocks, localVariableTable);
 
-            // TODO: Insert Phi functions
+            // Insert Phi functions
+            InsertPhiFunctions(postOrder);
 
             // TODO: Rename local variables
 
             // TODO: Print SSA form
+        }
+
+        private void InsertPhiFunctions(IList<BasicBlock> postOrderBlocks)
+        {
+            // From https://www.cs.cornell.edu/courses/cs6120/2020fa/lesson/5/
+
+            // for v in vars:
+            //    for d in Defs[v]:  # Blocks where v is assigned.
+            //       for block in DF[d]:  # Dominance frontier.
+            //          Add a ϕ - node to block,
+            //            unless we have done so already.
+            //          Add block to Defs[v](because it now writes to v!),
+            //            unless it's already in there.
+
+            // Compute the Dominance frontiers
+            var dominanceFrontierMap = ComputeDominanceFrontiers(postOrderBlocks);
+
+            for (int i = 0; i < postOrderBlocks.Count; i++)
+            {
+                var block = postOrderBlocks[i];
+
+                var iteratedDominanceFrontier = ComputeIteratedDominanceFrontier(block, dominanceFrontierMap);
+
+                if (iteratedDominanceFrontier.Count == 0)
+                {
+                    continue;
+                }
+
+                // Loop over variables that are assigned to in the block e.g. the def's
+                foreach (var defVar in block.VarDef)
+                {
+                    foreach (var blockInDominanceFront in iteratedDominanceFrontier)
+                    {
+                        if (!blockInDominanceFront.LiveIn.IsMember(defVar))
+                        {
+                            continue;
+                        }
+
+                        // TODO: Insert Phi functions
+                        /*
+                        if (!GetPhiNode(blockInDominanceFront, defVar))
+                        {
+                            InsertPhi(blockInDominanceFront, defVar);
+                        }
+                        */
+                    }
+                }
+            }
+        }
+
+        private IList<BasicBlock> ComputeIteratedDominanceFrontier(BasicBlock block, IDictionary<BasicBlock, ISet<BasicBlock>> dominanceFrontierMap)
+        {
+            // The iterated dominance frontier of block B is the smallest set that includes
+            // B's dominance frontier and also includes the dominance frontier of all elements
+            // of the set
+
+            var iteratedDominanceFrontier = new List<BasicBlock>();
+
+            if (dominanceFrontierMap.TryGetValue(block, out var dominanceFrontier))
+            {
+                // Start by adding the dominance frontier of block to the iterated dominance frontier
+                iteratedDominanceFrontier.AddRange(dominanceFrontier);
+
+                for (var i = 0; i < iteratedDominanceFrontier.Count; i++)
+                {
+                    var f = iteratedDominanceFrontier[i];
+
+                    if (dominanceFrontierMap.TryGetValue(f, out var fDominanceFrontier))
+                    {
+                        foreach (var ff in fDominanceFrontier)
+                        {
+                            if (!iteratedDominanceFrontier.Contains(ff))
+                            {
+                                iteratedDominanceFrontier.Add(ff);
+                            }
+                        }
+                    }
+                }
+            }
+
+            var sb = new StringBuilder($"IDF({block.Label}) := {{");
+            int index = 0;
+            foreach (var f in iteratedDominanceFrontier)
+            {
+                if (index++ != 0) sb.Append(',');
+                sb.Append($"{f.Label}");
+            }
+            sb.Append("}}");
+            _logger.LogDebug(sb.ToString());
+
+            return iteratedDominanceFrontier;
+        }
+
+        private IDictionary<BasicBlock, ISet<BasicBlock>> ComputeDominanceFrontiers(IList<BasicBlock> postOrderBlocks)
+        {
+            // From "A simple, fast dominance algorithm", by Cooper, Harvey, and Kennedy.
+
+            // Recall that the dominance frontier of a block B is the set of blocks
+            // B3 such that there exists some B2 s.t. B3 is a successor of B2, and
+            // B dominates B2. Note that this dominance need not be strict -- B2
+            // and B may be the same node.
+
+            var dominanceFrontierMap = new Dictionary<BasicBlock, ISet<BasicBlock>>();
+
+            _logger.LogDebug("Computing Dominance Frontiers");
+
+            for (int i = 0; i < postOrderBlocks.Count; i++)
+            {
+                var block = postOrderBlocks[i];
+
+                _logger.LogDebug("Considering block {BlockLabel}", block.Label);
+
+                // Block B3 is in the dominance frontier of B1 if there is a B2
+                // such that B1 dominates B2 and B1 does not dominate B3 and B3
+                // is an immediate successor of B2.
+                // Note that B1 might be the same block as B2
+
+                // Here block is B3, and we are trying to find the B1's
+
+                // First we consider the predecessors of block looking for candidate B2's
+                // block is obviously an immediate successor of its immediate predecessors
+
+                var predecessors = block.Predecessors;
+
+                // If there is zero or one predecessors then there is no predecessor,
+                // or else the single predecessor dominates block, so no B2 exists
+                if (predecessors.Count <= 1)
+                {
+                    _logger.LogDebug("   Has {predecessorCount} predecessors; skipping", predecessors.Count);
+                    continue;
+                }
+
+                // We have more than one predecessor, each is a potential candidate for B2
+                // unless it dominates block
+
+                foreach (var predecessor in predecessors)
+                {
+                    // For each B2 consider the possible B1's by starting with B2 since a block dominates itself.
+                    // Then traverse upwards in the dominator tree stopping if we reach the root or the
+                    // immediate dominator of block
+                    // Along the way we make block part of the dominance frontier of B1
+                    for (var b1 = predecessor; b1 != null && b1 != block.ImmediateDominator; b1 = b1.ImmediateDominator)
+                    {
+                        _logger.LogDebug("      Adding {b1Label} to dominance frontier of predecessor dominator {predecessorDominator}", block.Label, b1.Label);
+
+                        if (!dominanceFrontierMap.TryGetValue(b1, out var dominanceFrontierBlocks))
+                        {
+                            dominanceFrontierBlocks = new HashSet<BasicBlock>();
+                            dominanceFrontierMap[b1] = dominanceFrontierBlocks;
+                        }
+
+                        dominanceFrontierBlocks.Add(block);
+                    }
+                }
+            }
+
+            _logger.LogDebug("Computed Dominance Frontier");
+            for (int i = 0; i < postOrderBlocks.Count; i++)
+            {
+                var block = postOrderBlocks[i];
+
+                var sb = new StringBuilder($"Block {block.Label} := {{");
+
+                if (dominanceFrontierMap.TryGetValue(block, out var dominanceFrontier))
+                {
+                    var index = 0;
+                    foreach (var dominanceFrontierBlock in dominanceFrontier)
+                    {
+                        if (index++ != 0) sb.Append(',');
+                        sb.Append($"{dominanceFrontierBlock.Label}");
+                    }
+                }
+                sb.Append("}}");
+                _logger.LogDebug(sb.ToString());
+            }
+
+            return dominanceFrontierMap;
         }
 
         private void LocalVarLiveness(IList<BasicBlock> blocks, IList<LocalVariableDescriptor> localVariableTable)
@@ -96,13 +274,13 @@ namespace ILCompiler.Compiler
             if (node is ILocalVariable localVarNode)
             {
                 MarkUseDef(localVarNode, useSet, defSet);
-            }            
+            }
         }
 
         private static void MarkUseDef(ILocalVariable tree, VariableSet useSet, VariableSet defSet)
         {
             // Assignment is a definition, everything else is a use.
-            
+
             // Should we also check for StoreIndEntry which is generated from Stfld import??
             // Are these really partial definitions e.g. struct field is assigned to s.f = ...
 
