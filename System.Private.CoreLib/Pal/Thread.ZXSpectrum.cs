2328b0f5eca142df0a5515df510a553e0ed88576
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2023-08-31 16:19:03 +0100

    Implement ZX Spectrum Thread.Sleep

--- System.Private.CoreLib/Pal/Thread.ZXSpectrum.cs
+++ System.Private.CoreLib/Pal/Thread.ZXSpectrum.cs
@@ -4,8 +4,21 @@ namespace System.Threading
 {
     public class Thread
     {
+        // This delays by approx 20 milliseconds for each delay unit
+        [DllImport(Libraries.Runtime, EntryPoint = "Delay")]
+        private static unsafe extern void Delay(int delay);
+
         public static void Sleep(int delayMs)
-        { 
+        {
+            var delay = delayMs / 20;
+
+            while (delay > 65535)
+            {
+                Delay(65535);
+                delay -= 65535;
+            }
+
+            Delay(delay);
         }
     }
 }
