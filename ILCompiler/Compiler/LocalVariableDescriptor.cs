fe2f70021bbd4e9afc087ee254e7a84aa4f7bb7e
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2023-09-09 16:58:00 +0100

    Extend Local Variable descriptors to include: * If they are tracked or not * If they are in SSA or not * If they are address exposed or not * PerSsaData used in Ssa Construction

--- ILCompiler/Compiler/LocalVariableDescriptor.cs
+++ ILCompiler/Compiler/LocalVariableDescriptor.cs
@@ -1,4 +1,5 @@
-﻿using ILCompiler.Common.TypeSystem.IL;
+﻿using ILCompiler.Compiler.Lowerings;
+using ILCompiler.Compiler.Ssa;
 
 namespace ILCompiler.Compiler
 {
@@ -17,5 +18,14 @@ namespace ILCompiler.Compiler
         public string Name = string.Empty;
 
         public bool MustInit { get; set; } = false;
+        public bool Tracked { get; set; } = false;
+
+        public bool AddressExposed { get; set; } = false;
+
+        public bool InSsa { get; set; } = false;
+
+        public SsaDefList<LocalSsaVariableDescriptor> PerSsaData { get; set; } = new SsaDefList<LocalSsaVariableDescriptor>();
+
+        public LocalSsaVariableDescriptor GetPerSsaData(int ssaNumber) => PerSsaData.SsaDefinition(ssaNumber);
     }
 }
---
+++ ILCompiler/Compiler/Ssa/LocalSsaVariableDescriptor.cs
@@ -0,0 +1,26 @@
+﻿namespace ILCompiler.Compiler.Ssa
+{
+    public class LocalSsaVariableDescriptor
+    {
+        public BasicBlock Block { get; set; }
+
+        public bool HasGlobalUse { get; private set; } = false;
+
+        public int NumberOfUses { get; private set; } = 0;
+
+        public LocalSsaVariableDescriptor(BasicBlock block)
+        {
+            Block = block;
+        }
+
+        public void AddUse(BasicBlock block)
+        {
+            if (block != Block)
+            {
+                HasGlobalUse = true;
+            }
+
+            NumberOfUses++;
+        }
+    }
+}
