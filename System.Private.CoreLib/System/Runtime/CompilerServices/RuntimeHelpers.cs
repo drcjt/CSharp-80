127e558b3fbff6702b40290d262feb40cd9f5597
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2023-10-01 16:31:51 +0100

    Add helper to check is type has explicit cctor

--- System.Private.CoreLib/System/Runtime/CompilerServices/RuntimeHelpers.cs
+++ System.Private.CoreLib/System/Runtime/CompilerServices/RuntimeHelpers.cs
@@ -1,7 +1,10 @@
 ﻿namespace System.Runtime.CompilerServices
 {
-    public class RuntimeHelpers
+    public static class RuntimeHelpers
     {
         public static unsafe int OffsetToStringData => 0; //sizeof(IntPtr) + sizeof(int);
+
+        public unsafe static bool HasCctor<T>() => EETypePtr.EETypePtrOf<T>()->GetFlags() == 1;
+
     }
 }
