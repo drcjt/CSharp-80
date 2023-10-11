808979083dbf464fc398cd4be0a0ae679b8fe419
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2023-10-11 20:42:32 +0100

    Add some simple virtual method tests (#380)

---
+++ Tests/Methodical/VirtualMethods/VirtualMethods.cs
@@ -0,0 +1,42 @@
+﻿namespace VirtualMethods
+{
+    public class BaseClass
+    {
+        public virtual int F1() => 1;
+        public virtual int F2() => 20;
+    }
+
+    public class  DerivedClass : BaseClass
+    {
+        public override int F1() => base.F1() + 1;
+        public new virtual int F2() => 30;
+    }
+
+    public static class Test
+    {
+        public static int Main()
+        {
+            var baseClass = new BaseClass();
+            if (baseClass.F1() != 1)
+            {
+                return 1;
+            }
+            if (baseClass.F2() != 20)
+            {
+                return 1;
+            }
+            
+            var derivedClass = new DerivedClass();
+            if (derivedClass.F1() != 2) 
+            {
+                return 1;
+            }
+            if (derivedClass.F2() != 30)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
