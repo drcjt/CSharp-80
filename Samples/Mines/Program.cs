8f8bf099b0006e01edda1b508904d6a1c2dc8814
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2023-11-09 21:39:38 +0000

    Add minesweeper sample (#400)
    * Add minesweeper sample
    
    * Refactoring and fixing code smells
    
    * More refactoring
    Add level selection

---
+++ Samples/Mines/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Mines
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Beginner (1), Intermediate (2), or Expert (3)?");
+            var level = int.Parse(Console.ReadLine());
+
+            if (level == 1)
+            {
+                var game = new Game(8, 8, 10);
+                game.Play();
+            } 
+            else if (level == 2)
+            {
+                var game = new Game(11, 11, 24);
+                game.Play();
+            }
+            else if (level == 3)
+            {
+                var game = new Game(14, 14, 45);
+                game.Play();
+            }
+        }
+    }
+}
---
+++ Samples/Mines/Square.cs
@@ -0,0 +1,35 @@
+﻿namespace Mines
+{
+    public class Square
+    {
+        public int X { get; }
+        public int Y { get; }
+        public int AdjacentMines { get; internal set; } = 0;
+
+        public bool Mined { get; internal set; } = false;
+        public bool Revealed { get; internal set; } = false;
+        public bool Flagged { get; internal set; } = false;
+
+        public Square(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Reset()
+        {
+            AdjacentMines = 0;
+            Mined = false;
+            Revealed = false;
+            Flagged = false;
+        }
+
+        public char AsChar()
+        {
+            if (Flagged) return 'F';
+            if (!Revealed) return ' ';
+            if (Mined) return '*';
+            return (char)('0' + AdjacentMines);
+        }
+    }
+}
