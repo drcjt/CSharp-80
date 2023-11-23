2d4b0e8fb471b51ad226ceb377b91a80fa5b3818
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2023-11-23 22:29:01 +0000

    Mark constant as contained when storing to local variable. (#411)
    Allows code gen to embed the constant into the instructions avoiding use of stack to hold constant.

---
+++ ILCompiler/Compiler/Lowerings/StoreLocalVariableLowering.cs
@@ -0,0 +1,19 @@
+﻿using ILCompiler.Compiler.EvaluationStack;
+
+namespace ILCompiler.Compiler.Lowerings
+{
+    internal class StoreLocalVariableLowering : ILowering<StoreLocalVariableEntry>
+    {
+        public StackEntry? Lower(StoreLocalVariableEntry entry)
+        {
+            if (entry.Op1.IsIntCnsOrI())
+            {
+                // When storing constant mark as contained as we can embed the constant
+                // into the instructions to store to the local variable directly
+                entry.Op1.Contained = true;
+            }
+
+            return null;
+        }
+    }
+}
