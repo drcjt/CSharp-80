37a26716e4d2a48eebb0fc128710f4649176deaf
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2024-01-01 16:36:42 +0000

    Add simple exception test for try with no throw Enable exceptions for method tests

---
+++ Tests/Methodical/Exceptions/SimpleExceptionTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Exceptions
+{
+    internal static class SimpleExceptionTests
+    {
+        private static int Try_NoThrow()
+        {
+            int result;
+
+            try
+            {
+                result = 0;
+            }
+            catch
+            {
+                result = 1;
+            }
+
+            return result;
+        }
+
+        public static int RunTests()
+        {
+            return Try_NoThrow();
+        }
+    }
+}
