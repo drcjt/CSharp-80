eea71761c0641bb1a2cbff84d431c7f1410dc1da
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2024-01-05 21:05:20 +0000

    Add managed methods for native EH routines

---
+++ System.Private.CoreLib/System/Runtime/InternalCalls.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace System.Runtime
+{
+    internal static class InternalCalls
+    {
+        [DllImport(Libraries.Runtime, EntryPoint = "CALLCATCHHANDLER")]
+        internal static extern unsafe void CallCatchHandler(object exception, byte* pCatchHandler, ref ExInfo exceptionInfo);
+
+        [DllImport(Libraries.Runtime, EntryPoint = "EHENUMINIT")]
+        internal static extern unsafe void EHEnumInit(void* pEHEnum);
+
+        [DllImport(Libraries.Runtime, EntryPoint = "EHENUMNEXT")]
+        internal static extern unsafe bool EHEnumNext(void* pEHEnum, void* pEHClause);
+    }
+}
