f071c0351accc16d214c61b08f15f2bf480f6191
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2024-01-30 21:17:36 +0000

    Improve test coverage

--- Tests/Directed/ConstantFolding/ConstantFolding.cs
+++ Tests/Directed/ConstantFolding/ConstantFolding.cs
@@ -5,13 +5,18 @@
         public static int Main()
         {
             int result = 0;
-            result += TestSubtractionMorphToAddition(25, 20);
-            result += TestAdditionWithMultipleConstants(13, 90);
-            result += TestMultiplicationWithMultipleConstants(13, 4680);
+            result += TestIntSubtractionMorphToAddition(25, 20);
+            result += TestIntAdditionWithMultipleConstants(13, 90);
+            result += TestIntMultiplicationWithMultipleConstants(13, 4680);
+
+            result += TestNIntSubtractionMorphToAddition(25, 20);
+            result += TestNIntAdditionWithMultipleConstants(13, 90);
+            result += TestNIntMultiplicationWithMultipleConstants(13, 4680);
+
             return result;
         }
 
-        private static int TestSubtractionMorphToAddition(int value, int expected)
+        private static int TestNIntSubtractionMorphToAddition(nint value, nint expected)
         {
 
             if (value - 5 != expected)
@@ -22,7 +27,7 @@
             return 0;
         }
 
-        private static int TestAdditionWithMultipleConstants(int value, int expected)
+        private static int TestNIntAdditionWithMultipleConstants(nint value, nint expected)
         {
             if (value + 5 + 72 != expected)
             {
@@ -32,7 +37,38 @@
             return 0;
         }
 
-        private static int TestMultiplicationWithMultipleConstants(int value, int expected)
+        private static int TestNIntMultiplicationWithMultipleConstants(nint value, nint expected)
+        {
+            if (value * 5 * 72 != expected)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        private static int TestIntSubtractionMorphToAddition(int value, int expected)
+        {
+
+            if (value - 5 != expected)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        private static int TestIntAdditionWithMultipleConstants(int value, int expected)
+        {
+            if (value + 5 + 72 != expected)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        private static int TestIntMultiplicationWithMultipleConstants(int value, int expected)
         {
             if (value * 5 * 72 != expected)
             {
