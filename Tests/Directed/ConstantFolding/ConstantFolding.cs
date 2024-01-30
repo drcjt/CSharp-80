a56b78a4c2555a9410f66dff282af2788315c2bd
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2024-01-30 20:24:52 +0000

    Add some constant folding into morphing. Basic idea is to move constants to the right and then pattern match e.g. X op C1 op C2.
    Results in minor improvement in samples by reducing code size and improving overall performance

---
+++ Tests/Directed/ConstantFolding/ConstantFolding.cs
@@ -0,0 +1,44 @@
+﻿namespace ConstantFolding
+{
+    public static class Tests
+    {
+        public static int Main()
+        {
+            TestSubtractionMorphToAddition(25, 20);
+            TestAdditionWithMultipleConstants(13, 90);
+            TestMultiplicationWithMultipleConstants(13, 4680);
+            return 0;
+        }
+
+        private static int TestSubtractionMorphToAddition(int value, int expected)
+        {
+
+            if (value - 5 != expected)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        private static int TestAdditionWithMultipleConstants(int value, int expected)
+        {
+            if (value + 5 + 72 != expected)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        private static int TestMultiplicationWithMultipleConstants(int value, int expected)
+        {
+            if (value * 5 * 72 != expected)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
