1fe3399b2d3e8f664ef3b7852babeee6a39ffda4
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2024-02-19 19:58:05 +0000

    Add tests to verify EEtype.RelatedType is correctly set for classes.

--- Tests/CoreLib/CoreLib/Assert.cs
+++ Tests/CoreLib/CoreLib/Assert.cs
@@ -35,5 +35,37 @@ namespace CoreLib
                 Environment.Exit(1);
             }
         }
+
+        public static void AreEquals(object expected, object actual)
+        {
+            if (!expected.Equals((object)actual))
+            {
+                Environment.Exit(1);
+            }
+        }
+
+        public static void IsFalse(bool condition)
+        {
+            if (condition)
+            {
+                Environment.Exit(1);
+            }
+        }
+
+        public static void IsTrue(bool condition)
+        {
+            if (!condition)
+            {
+                Environment.Exit(1);
+            }
+        }
+
+        public static void IsNotNull(object obj)
+        {
+            if (obj == null)
+            {
+                Environment.Exit(1);
+            }
+        }
     }
 }
---
+++ Tests/CoreLib/CoreLib/TypeCastTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CoreLib
+{
+    internal class SuperClass
+    {
+        public SuperClass() { }
+    }
+
+    internal class  SubClass : SuperClass
+    {
+        public SubClass() : base() { }
+    }
+
+    internal class TypeCastTests
+    {
+        public unsafe static void ClassTypeCastTests()
+        {
+            var super = new SuperClass();
+            var sub = new SubClass();
+
+            Assert.IsTrue(sub is SubClass);
+            Assert.IsTrue(sub is SuperClass);
+            Assert.IsTrue(super is SuperClass);
+            Assert.IsFalse(super is SubClass);
+            Assert.IsTrue(super is Object);
+            Assert.IsTrue(sub is Object);
+        }
+    }
+}
