1766849d049b688bb73ba40e67c694761398674c
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2024-02-28 22:40:58 +0000

    Add String.Equals so can use in tests for Interpolated String Handler

---
+++ System.Private.CoreLib/System/String.Comparison.cs
@@ -0,0 +1,44 @@
+﻿namespace System
+{
+    public sealed partial class String
+    {
+        public override bool Equals(object other)
+        {
+            if (this == other)
+                return true;
+
+            if (other is not string str)
+                return false;
+
+            if (this.Length != str.Length)
+                return false;
+
+            // Compare actual characters in the strings
+            return EqualsHelper(this, str);
+        }
+
+        private unsafe bool EqualsHelper(string strA, string strB)
+        {
+            fixed (char* strABuffer = strA)
+            {
+                fixed (char* strBBuffer = strB)
+                {
+                    char* pA = strABuffer;
+                    char* pB = strBBuffer;
+
+                    var offset = 0;
+                    while (offset < strA.Length)
+                    {
+                        if (*(pA++) != *(pB++))
+                        {
+                            return false;
+                        }
+                        offset++;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
