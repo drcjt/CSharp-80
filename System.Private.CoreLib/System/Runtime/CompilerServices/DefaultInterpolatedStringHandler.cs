cc31ee52d5861143ed1422569f5006d9896eb70c
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2024-02-28 22:31:41 +0000

    Add default interpolated string handler Just concats strings together

---
+++ System.Private.CoreLib/System/Runtime/CompilerServices/DefaultInterpolatedStringHandler.cs
@@ -0,0 +1,36 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    [InterpolatedStringHandler]
+    public class DefaultInterpolatedStringHandler
+    {
+        private string result = "";
+        public DefaultInterpolatedStringHandler(int literalLength, int formattedCount)
+        {
+        }
+
+        public void AppendLiteral(string value)
+        {
+            result += value;
+        }
+
+        public void AppendFormatted(int value)
+        {
+            result += value.ToString();
+        }
+
+        // TODO: This requires the constrained IL opcode to be implemented
+        /*
+        public void AppendFormatted<T>(T value)
+        {
+            result += value.ToString();
+        }
+        */
+
+        public string ToStringAndClear()
+        {
+            var builtString = result;
+            result = "";
+            return builtString;
+        }
+    }
+}
---
+++ System.Private.CoreLib/System/Runtime/CompilerServices/InterpolatedStringHandlerAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    public sealed class InterpolatedStringHandlerAttribute : Attribute
+    {
+        public InterpolatedStringHandlerAttribute() { }
+    }
+}
