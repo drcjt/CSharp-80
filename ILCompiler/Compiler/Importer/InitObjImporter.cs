1cefe3f2a5ac0bec393d8273835494a62820516c
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2024-03-04 20:37:35 +0000

    Use memset to implement initobj (#471)
    Add IL test for initobj

--- ILCompiler/Compiler/Importer/InitObjImporter.cs
+++ ILCompiler/Compiler/Importer/InitObjImporter.cs
@@ -1,4 +1,6 @@
-﻿using dnlib.DotNet.Emit;
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using ILCompiler.Compiler.EvaluationStack;
 using ILCompiler.Interfaces;
 
 namespace ILCompiler.Compiler.Importer
@@ -9,8 +11,19 @@ namespace ILCompiler.Compiler.Importer
         {
             if (instruction.OpCode.Code != Code.Initobj) return false;
 
-            // TODO: Need to implement this
-            importer.PopExpression();
+            var address = importer.PopExpression();
+
+            var typeSig = (instruction.Operand as ITypeDefOrRef).ToTypeSig();
+            typeSig = context.Method.ResolveType(typeSig);
+            int elemSize = typeSig.GetInstanceFieldSize();
+
+            var size = new Int32ConstantEntry(elemSize);
+            var initValue = new Int32ConstantEntry(0);
+            var args = new List<StackEntry>() { size, initValue, address };
+
+            var node = new CallEntry("Memset", args, VarType.Void, null);
+            importer.ImportAppendTree(node);
+
             return true;
         }
     }
