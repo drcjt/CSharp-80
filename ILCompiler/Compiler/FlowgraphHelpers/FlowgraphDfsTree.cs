2de7513db1e117f9f891ff63f0f24421b8dcf703
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2024-08-01 19:56:34 +0100

    Add code to find loops.

--- ILCompiler/Compiler/FlowgraphHelpers/FlowgraphDfsTree.cs
+++ ILCompiler/Compiler/FlowgraphHelpers/FlowgraphDfsTree.cs
@@ -1,55 +1,84 @@
 ﻿namespace ILCompiler.Compiler.FlowgraphHelpers
 {
-    public class FlowgraphDfsTree
+    public class FlowgraphDfsTree(IList<BasicBlock> postOrder, bool hasCycle)
     {
-        public IList<BasicBlock> PostOrder { get; init; }
+        public IList<BasicBlock> PostOrder { get; init; } = postOrder;
+        public bool HasCycle { get; init; } = hasCycle;
 
-        public FlowgraphDfsTree(IList<BasicBlock> postOrder)
+        public class AllSuccessorEnumerator(BasicBlock block)
         {
-            PostOrder = postOrder;
+            private int _position = 0;
+            public BasicBlock Block { get; init; } = block;
+
+            public BasicBlock? NextSuccessor => _position < Block.Successors.Count ? Block.Successors[_position++] : null;
+        }
+
+        public static bool IsAncestor(BasicBlock ancestor, BasicBlock descendant)
+        {
+            return ancestor.PreOrderNum <= descendant.PreOrderNum &&
+                descendant.PostOrderNum <= ancestor.PostOrderNum;
         }
 
         public static FlowgraphDfsTree Build(BasicBlock firstBlock)
         {
             var postOrder = new List<BasicBlock>();
+            bool hasCycle = false;
+
             var visitedBlocks = new HashSet<BasicBlock>();
 
-            var blocksStack = new Stack<BasicBlock>();
-            var successorsStack = new Stack<int>();
+            uint postOrderIndex = 0;
+            uint preOrderIndex = 0;
 
-            blocksStack.Push(firstBlock);
-            successorsStack.Push(0);
+            var blocks = new Stack<AllSuccessorEnumerator>();
+            blocks.Push(new AllSuccessorEnumerator(firstBlock));
+            VisitPreOrder(firstBlock, preOrderIndex++);
 
-            uint postIndex = 0;
-
-            while (blocksStack.Count > 0)
+            while (blocks.Count != 0)
             {
-                var topBlock = blocksStack.Peek();
-                var successorIndex = successorsStack.Pop();
+                var block = blocks.Peek().Block;
+                var successor = blocks.Peek().NextSuccessor;
 
-                if (successorIndex < topBlock.Successors.Count)
+                if (successor != null)
                 {
-                    var successor = topBlock.Successors[successorIndex];
-                    successorsStack.Push(successorIndex + 1);
-
                     if (!visitedBlocks.Contains(successor))
                     {
-                        blocksStack.Push(successor);
-                        successorsStack.Push(0);
-
+                        blocks.Push(new AllSuccessorEnumerator(successor));
                         visitedBlocks.Add(successor);
+
+                        VisitPreOrder(successor, preOrderIndex++);
                     }
+
+                    VisitEdge(block, successor, ref hasCycle);
                 }
                 else
                 {
-                    var block = blocksStack.Pop();
-                    block.PostOrderNum = postIndex;
-                    postOrder.Add(block);
-                    postIndex++;
+                    blocks.Pop();
+                    VisitPostOrder(block, postOrderIndex++, postOrder);
                 }
             }
 
-            return new FlowgraphDfsTree(postOrder);
+            return new FlowgraphDfsTree(postOrder, hasCycle);
+        }
+
+        private static void VisitPreOrder(BasicBlock block, uint preOrderIndex)
+        {
+            block.PreOrderNum = preOrderIndex;
+            block.PostOrderNum = uint.MaxValue;
+        }
+
+        private static void VisitPostOrder(BasicBlock block, uint postOrderIndex, List<BasicBlock> postOrder)
+        {
+            block.PostOrderNum = postOrderIndex;
+            postOrder.Add(block);
+        }
+
+        private static void VisitEdge(BasicBlock block, BasicBlock successor, ref bool hasCycle)
+        {
+            // Check if block -> successor is a back edge, if so then the graph has a cycle
+            if ((successor.PreOrderNum <= block.PreOrderNum) && successor.PostOrderNum == uint.MaxValue)
+            {
+                hasCycle = true;
+            }
         }
     }
 }
--- ILCompiler/Compiler/LoopFinder.cs
+++ ILCompiler/Compiler/LoopFinder.cs
@@ -1,18 +1,31 @@
 ﻿using ILCompiler.Compiler.FlowgraphHelpers;
 using ILCompiler.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace ILCompiler.Compiler
 {
     public class LoopFinder : ILoopFinder
     {
-        public void FindLoops(IList<BasicBlock> blocks)
+        private readonly ILogger<LoopFinder> _logger;
+        public LoopFinder(ILogger<LoopFinder> logger) 
         {
-            var reversePostOrder = FlowgraphDfsTree.Build(blocks[0]).PostOrder.Reverse();
-            ComputeReachabilitySets(reversePostOrder);
+            _logger = logger;
         }
 
-        private static void ComputeReachabilitySets(IEnumerable<BasicBlock> reversePostOrder)
+        public void FindLoops(IList<BasicBlock> blocks)
         {
+            var dfs = FlowgraphDfsTree.Build(blocks[0]);
+
+            var loops = FlowGraphNaturalLoops.Find(dfs, _logger);
+
+            // Consider moving out of here
+            ComputeReachabilitySets(dfs);
+        }
+
+        private static void ComputeReachabilitySets(FlowgraphDfsTree dfs)
+        {
+            var reversePostOrder = dfs.PostOrder.Reverse();
+
             // Every block can reach itself
             foreach (var block in reversePostOrder)
             {
@@ -27,6 +40,198 @@ namespace ILCompiler.Compiler
                     block.Reach.UnionWith(predecessor.Reach);
                 }
             }
+        }
+    }
+
+    public class FlowGraphNaturalLoop(FlowgraphDfsTree dfsTree, BasicBlock header)
+    {
+        public FlowgraphDfsTree DfsTree { get; init; } = dfsTree;
+        public BasicBlock Header { get; } = header;
+        public IList<FlowEdge> BackEdges { get; } = [];
+        public IList<FlowEdge> ExitEdges { get; } = [];
+        public IList<FlowEdge> EntryEdges { get; } = [];
+        public FlowGraphNaturalLoop? Parent { get; set; }
+
+        public IList<BasicBlock> Blocks { get; } = [];
+
+        public bool VisitLoopBlocksReversePostOrder(Func<BasicBlock, bool> visitLoopBlock)
+        {
+            foreach (var block in Blocks.OrderByDescending(x => x.PostOrderNum))
+            {
+                if (!visitLoopBlock(block))
+                    return false;
+            }
+            return true;
+        }
+
+        public bool ContainsBlock(BasicBlock block)
+        {
+            if (!DfsTree.PostOrder.Contains(block))
+                return false;
+
+            return Blocks.Contains(block);
+        }
+    }
+
+    public class FlowGraphNaturalLoops(FlowgraphDfsTree dfsTree)
+    {
+        private IList<FlowGraphNaturalLoop> _loops = [];
+
+        public IEnumerable<FlowGraphNaturalLoop> InPostOrder => _loops.Reverse();
+
+        public void Add(FlowGraphNaturalLoop loop)
+        {
+            _loops.Add(loop);
+        }
+
+        public static FlowGraphNaturalLoops Find(FlowgraphDfsTree dfsTree, ILogger<LoopFinder> logger)
+        {
+            logger.LogDebug("Finding loops in DFS tree");
+            logger.LogDebug("Reverse Post Order -> Block [pre, post]");
+
+            for (int i = dfsTree.PostOrder.Count; i != 0; i--)
+            {
+                var rpoNumber = dfsTree.PostOrder.Count - i;
+                var block = dfsTree.PostOrder[i-1];
+                logger.LogDebug($"{rpoNumber} -> {block.Label} [{block.PreOrderNum}, {block.PostOrderNum}]");
+            }
+
+            var loops = new FlowGraphNaturalLoops(dfsTree);
+
+            // Check if tree has any cycles if not skip
+            if (!dfsTree.HasCycle)
+            {
+                logger.LogDebug("Flow graph has no cycles. Skipping identification of natural loops");
+
+                return loops;
+            }
+
+            foreach (var header in dfsTree.PostOrder.Reverse())
+            {
+                FlowGraphNaturalLoop? loop = null;
+
+                // If a block is a ancestor of one of its predecessors then the block is a loop header
+                foreach (var predecessorBlock in header.Predecessors)
+                {
+                    if (dfsTree.PostOrder.Contains(predecessorBlock) && FlowgraphDfsTree.IsAncestor(header, predecessorBlock))
+                    {
+                        loop = loop ?? new FlowGraphNaturalLoop(dfsTree, header);
+                        loop.BackEdges.Add(new FlowEdge(predecessorBlock, header));
+
+                        logger.LogDebug($"{predecessorBlock.Label} -> {header.Label} is a backedge");
+                    }
+                }
+
+                if (loop == null)
+                    continue;
+
+                logger.LogDebug($"{header.Label} is the header of a DFS loop with {loop.BackEdges.Count} backedges");
+
+                // Walk backwards in flow along the back edges from header to determine if this 
+                // is a natural loop and to find all of the basic blocks in the loop
+                var worklist = new Stack<BasicBlock>();
+                if (!FindNaturalLoopBlocks(loop, worklist, logger))
+                {
+                    // Improper loop
+                    logger.LogDebug("Loop contains an improper loop header");
+                    continue;
+                }
+
+                logger.LogDebug($"Loop has {loop.Blocks.Count} blocks");
+
+                // Find the exit edges
+                loop.VisitLoopBlocksReversePostOrder(loopBlock =>
+                {
+                    foreach (var successor in loopBlock.Successors)
+                    {
+                        if (!loop.ContainsBlock(successor))
+                        {
+                            FlowEdge exitEdge = new FlowEdge(loopBlock, successor);
+                            loop.ExitEdges.Add(exitEdge);
+
+                            logger.LogDebug($"{loopBlock.Label} -> {successor.Label} is an exit edge");
+                        }
+                    }
+                    return true;
+                });
+
+                // Find the entry edges
+                foreach (var predecessor in header.Predecessors)
+                {
+                    if (dfsTree.PostOrder.Contains(predecessor) && FlowgraphDfsTree.IsAncestor(header, predecessor))
+                    {
+                        loop.EntryEdges.Add(new FlowEdge(predecessor, header));
+
+                        logger.LogDebug($"{predecessor.Label} -> {header.Label} is an entry edge");
+                    }
+                }
+
+                // Search for parent loop
+                foreach (var otherLoop in loops.InPostOrder)
+                {
+                    if (otherLoop.ContainsBlock(header))
+                    {
+                        loop.Parent = otherLoop;
+
+                        logger.LogDebug($"Nested within loop starting at {otherLoop.Header.Label}");
+
+                        break;
+                    }
+                }
+
+                loops.Add(loop);
+
+                logger.LogDebug($"Added loop with header {loop.Header.Label}");
+            }
+
+            return loops;
+        }
+
+        private static bool FindNaturalLoopBlocks(FlowGraphNaturalLoop loop, Stack<BasicBlock> worklist, ILogger<LoopFinder> logger)
+        {
+            var dfsTree = loop.DfsTree;
+            loop.Blocks.Add(loop.Header);
+
+            worklist.Clear();
+            foreach (var backEdge in loop.BackEdges)
+            {
+                var backEdgeSource = backEdge.Source;
+
+                if (backEdgeSource == loop.Header)
+                    continue;
+
+                worklist.Push(backEdgeSource);
+                loop.Blocks.Add(backEdgeSource);
+            }
+
+            // Work backwards through the flowgraph to the loop head or to
+            // another predecessor that is outside of the loop
+            while (worklist.Count != 0)
+            {
+                var loopBlock = worklist.Pop();
+
+                foreach (var predecessorBlock in loopBlock.Predecessors)
+                {
+                    if (!dfsTree.PostOrder.Contains(predecessorBlock))
+                        continue;
+
+                    // The header block cannot dominate the predecessor block unless it is an ancestor
+                    if (!FlowgraphDfsTree.IsAncestor(loop.Header, predecessorBlock))
+                    {
+                        logger.LogDebug($"Loop is not natural based on {predecessorBlock.Label} -> {loopBlock.Label}");
+                        
+                        return false;
+                    }
+
+                    if (!loop.Blocks.Contains(predecessorBlock))
+                    {
+                        loop.Blocks.Add(predecessorBlock);
+                        worklist.Push(predecessorBlock);
+                    }
+                }
+            }
+
+            return true;
         }
     }
 }
