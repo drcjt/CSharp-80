b7d3bcb51ce5271e95d9405383bf90bd1a9d7722
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2024-08-19 22:20:18 +0100

    Add more integer performance benchmarks

---
+++ Tests/Performance/CodeQuality/Benchmarks/Fib/Fib.cs
@@ -0,0 +1,30 @@
+﻿namespace Benchmarks
+{
+    public static class Fib
+    {
+        const int Number = 24;
+
+        static int Fibonacci(int x)
+        {
+            if (x > 2)
+            {
+                return (Fibonacci(x - 1) + Fibonacci(x - 2));
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        static bool Bench()
+        {
+            int fib = Fibonacci(Number);
+            return (fib == 46368);
+        }
+
+        public static int Main()
+        {
+            return Bench() ? 0 : 1;
+        }
+    }
+}
---
+++ Tests/Performance/CodeQuality/Benchmarks/Pi/Pi.cs
@@ -0,0 +1,52 @@
+﻿namespace Benchmarks
+{
+    public static class Pi
+    {
+        static int[] ComputePi(int[] a)
+        {
+            //const int d = 4;
+            const int r = 10000;
+            const int n = 25;
+            const int m = 332; // (int)(3.322 * n * d);
+            int[] digits = new int[n];
+            int i, k, q;
+
+            for (i = 0; i <= m; i++)
+            {
+                a[i] = 2;
+            }
+
+            a[m] = 4;
+
+            for (i = 1; i <= n; i++)
+            {
+                q = 0;
+                for (k = m; k > 0; k--)
+                {
+                    a[k] = a[k] * r + q;
+                    q = a[k] / (2 * k + 1);
+                    a[k] -= (2 * k + 1) * q;
+                    q *= k;
+                }
+                a[0] = a[0] * r + q;
+                q = a[0] / r;
+                a[0] -= q * r;
+                digits[i - 1] = q;
+            }
+
+            return digits;
+        }
+
+        static bool Bench()
+        {
+            int[] a = new int[333];
+            int[] digits = ComputePi(a);
+            return (digits[0] == 3 && digits[1] == 1415 && digits[2] == 9265 && digits[24] == 2117);
+        }
+
+        public static int Main()
+        {
+            return Bench() ? 0 : 1;
+        }
+    }
+}
