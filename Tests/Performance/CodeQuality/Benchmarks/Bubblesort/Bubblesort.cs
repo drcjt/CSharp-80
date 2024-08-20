c27288bff8885db70cc5347029647b3165f6cc79
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2024-08-20 20:54:12 +0100

    Add more integer performance benchmarks (#515)
    * Add more integer performance benchmarks
    
    * Add sort benchmark projects to solution file

---
+++ Tests/Performance/CodeQuality/Benchmarks/Bubblesort/Bubblesort.cs
@@ -0,0 +1,70 @@
+﻿namespace Benchmarks
+{
+    public static class Bubblesort
+    {
+        private static int _seed = 7;
+        static int Random(int size)
+        {
+            unchecked
+            {
+                _seed = _seed * 25173 + 13849;
+            }
+            return (_seed % size);
+        }
+
+        static void SortArray(int[] tab, int last)
+        {
+            bool swap;
+            int temp;
+            do
+            {
+                swap = false;
+                for (int i = 0; i < last; i++)
+                {
+                    if (tab[i] > tab[i + 1])
+                    {
+                        temp = tab[i];
+                        tab[i] = tab[i + 1];
+                        tab[i + 1] = temp;
+                        swap = true;
+                    }
+                }
+            }
+            while (swap);
+        }
+
+        const int MaxNum = 100;
+        const int Modulus = 0x20000;
+        static bool Bench()
+        {
+            int[] buffer = new int[MaxNum];
+
+            for (int j = 0; j < MaxNum; ++j)
+            {
+                int temp = Random(Modulus);
+                if (temp < 0)
+                {
+                    temp = (-temp);
+                }
+                buffer[j] = temp;
+            }
+
+            SortArray(buffer, MaxNum - 1);
+
+            for (int j = 0; j < MaxNum - 1; ++j)
+            {
+                if (buffer[j] > buffer[j + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static int Main()
+        {
+            return Bench() ? 0 : 1;
+        }
+    }
+}
---
+++ Tests/Performance/CodeQuality/Benchmarks/Quicksort/Quicksort.cs
@@ -0,0 +1,89 @@
+﻿namespace Benchmarks
+{
+    public static class Quicksort
+    {
+        private static int _seed = 7;
+        static int Random(int size)
+        {
+            unchecked
+            {
+                _seed = _seed * 25173 + 13849;
+            }
+            return (_seed % size);
+        }
+
+        static void Quick(int lo, int hi, int[] arr)
+        {
+
+            int i, j;
+            int pivot, temp;
+
+            if (lo < hi)
+            {
+                for (i = lo, j = hi, pivot = arr[hi]; i < j;)
+                {
+                    while (i < j && arr[i] <= pivot)
+                    {
+                        ++i;
+                    }
+                    while (j > i && arr[j] >= pivot)
+                    {
+                        --j;
+                    }
+                    if (i < j)
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+
+                // need to swap the pivot and a[i](or a[j] as i==j) so
+                // that the pivot will be at its final place in the sorted array
+
+                if (i != hi)
+                {
+                    temp = arr[i];
+                    arr[i] = pivot;
+                    arr[hi] = temp;
+                }
+                Quick(lo, i - 1, arr);
+                Quick(i + 1, hi, arr);
+            }
+        }
+
+        const int MaxNum = 500;
+        const int Modulus = 0x20000;
+        static bool Bench()
+        {
+            int[] buffer = new int[MaxNum];
+
+            for (int j = 0; j < MaxNum; ++j)
+            {
+                int temp = Random(Modulus);
+                if (temp < 0)
+                {
+                    temp = (-temp);
+                }
+                buffer[j] = temp;
+            }
+
+            Quick(0, MaxNum - 1, buffer);
+
+            for (int j = 0; j < MaxNum - 1; ++j)
+            {
+                if (buffer[j] > buffer[j + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static int Main()
+        {
+            return Bench() ? 0 : 1;
+        }
+    }
+}
