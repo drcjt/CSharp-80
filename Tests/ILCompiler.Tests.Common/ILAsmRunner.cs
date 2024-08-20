0c297e945a3d7aa91f0f5706ba6924db8e72bcbc
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2024-08-20 17:32:00 +0100

    Add integer perf benchmarks (#514)
    * Initial creation of performance tests focusing on integer benchmarks
    
    * Add more integer performance benchmarks
    
    * Fix to get ilasm working again.

--- Tests/ILCompiler.Tests.Common/ILAsmRunner.cs
+++ Tests/ILCompiler.Tests.Common/ILAsmRunner.cs
@@ -4,7 +4,7 @@
     {
         public static bool Assemble(string ilFileName)
         {
-            var ilAsmPath = @"%USERPROFILE%\.nuget\packages\microsoft.netcore.ilasm\6.0.0\runtimes\native\ilasm.exe";
+            var ilAsmPath = @"%USERPROFILE%\.nuget\packages\runtime.win-x64.microsoft.netcore.ilasm\8.0.0\runtimes\win-x64\native\ilasm.exe";
             ilAsmPath = Environment.ExpandEnvironmentVariables(ilAsmPath);
 
             return ProcessRunner.RunProcess(ilAsmPath, ilFileName);
---
+++ Tests/Performance/CodeQuality/Benchmarks/CSieve/CSieve.cs
@@ -0,0 +1,46 @@
+﻿namespace Benchmarks
+{
+    public static class CSieve
+    {
+        public const int Iterations = 1;
+        const int Size = 4095;
+
+        static bool Bench()
+        {
+            bool[] flags = new bool[Size + 1];
+            int count = 0;
+            for (int iter = 1; iter <= Iterations; iter++)
+            {
+                count = 0;
+
+                // Initially, assume all are prime
+                for (int i = 0; i <= Size; i++)
+                {
+                    flags[i] = true;
+                }
+
+                // Refine
+                for (int i = 2; i <= Size; i++)
+                {
+                    if (flags[i])
+                    {
+                        // Found a prime
+                        for (int k = i + i; k <= Size; k += i)
+                        {
+                            // Cancel its multiples
+                            flags[k] = false;
+                        }
+                        count++;
+                    }
+                }
+            }
+
+            return (count == 564);
+        }
+
+        public static int Main()
+        {
+            return Bench() ? 0 : 1;
+        }
+    }
+}
