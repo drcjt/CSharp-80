a57d3455c67af7073ad4cb19716ed1eebed3e8fa
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2024-09-01 10:30:11 +0100

    Simplify greater than equal routine to remove i_cmp (#521)
    Use 16 bit subtracts for i_gt and i_gt_un

--- ILCompiler/Runtime/i_gt.asm
+++ ILCompiler/Runtime/i_gt.asm
@@ -1,62 +1,26 @@
 ; 32 bit signed greater than comparison
 ; Entry: primary, secondary on stack
 ; Exit: Carry set if true
+
 i_gt:
+	POP HL
+	EXX
+
+	POP DE
 	POP BC
 
 	POP HL
-	POP DE
 
-	EXX
+	XOR A			; Clear carry flag
+	SBC HL, DE
 
-	POP HL		; LSW
-	POP DE		; MSW
+	EX DE, HL
 
-	LD A, L
+	POP HL
+	SBC HL, BC
 
-	EXX
-
-	PUSH BC
-
-	SUB L
-	LD L, A
-
-	EXX
 	LD A, H
-	EXX
-
-	SBC A, H
-	LD H, A
-
-	EXX
-	LD A, E
-	EXX
-
-	SBC A, E
-	LD E, A
-
-	EXX 
-	LD A, D
-	EXX
-
-	SBC A, D
-	LD D, A
-
 	ADD A, A
-	JR C, i_gt_1
 
-	LD A, H
-	OR L
-	OR D
-	OR E
-
-i_gt_1:
-	LD HL, 1
-
-	JP NC, i_gt_nc
-	RET
-
-i_gt_nc:
-	DEC L
-
-	RET
+	EXX
+	JP (HL)
--- ILCompiler/Runtime/i_gt_un.asm
+++ ILCompiler/Runtime/i_gt_un.asm
@@ -2,60 +2,21 @@
 ; Entry: primary, secondary on stack
 ; Exit: Carry set if true
 i_gt_un:
+	POP HL
+	EXX
+
+	POP DE
 	POP BC
 
 	POP HL
-	POP DE
+
+	XOR A			; Clear carry flag
+	SBC HL, DE
+
+	EX DE, HL
+
+	POP HL
+	SBC HL, BC
 
 	EXX
-
-	POP HL		; LSW
-	POP DE		; MSW
-
-	LD A, L
-
-	EXX
-
-	PUSH BC
-
-	SUB L
-	LD L, A
-
-	EXX
-	LD A, H
-	EXX
-
-	SBC A, H
-	LD H, A
-
-	EXX
-	LD A, E
-	EXX
-
-	SBC A, E
-	LD E, A
-
-	EXX 
-	LD A, D
-	EXX
-
-	SBC A, D
-	LD D, A
-
-	JR C, i_gt_un_1
-
-	LD A, H
-	OR L
-	OR D
-	OR E
-
-i_gt_un_1:
-	LD HL, 1
-
-	JP NC, i_gt_un_nc
-	RET
-
-i_gt_un_nc:
-	DEC L
-
-	RET
+	JP (HL)
