430d244d73c3740ba4c1f09f7d4ad58fbc500a0e
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2024-09-15 19:50:17 +0100

    Fixes virtual calls with arguments. These are passed on the stack in left to right order after the this parameter. The runtime VirtualCall routine expects the this pointer to be on top of the stack behind the return address - this fix adds the this pointer after the arguments and changes the VirtualCall routine to pop this off the stack to access the vtable. (#528)

--- Tests/Methodical/VirtualMethods/VirtualMethods.cs
+++ Tests/Methodical/VirtualMethods/VirtualMethods.cs
@@ -1,15 +1,21 @@
-﻿namespace VirtualMethods
+﻿using System.Runtime.CompilerServices;
+
+namespace VirtualMethods
 {
     public class BaseClass
     {
         public virtual int F1() => 1;
         public virtual int F2() => 20;
+
+        public virtual int F3(int a, int b, int c) => (a + b) * c;
     }
 
     public class  DerivedClass : BaseClass
     {
         public override int F1() => base.F1() + 1;
         public new virtual int F2() => 30;
+
+        public override int F3(int a, int b, int c) => a + (b * c);
     }
 
     public static class Test
@@ -25,6 +31,11 @@
             {
                 return 1;
             }
+
+            if (baseClass.F3(1, 2, 3) != 9)
+            {
+                return 1;
+            }
             
             var derivedClass = new DerivedClass();
             if (derivedClass.F1() != 2) 
@@ -32,6 +43,11 @@
                 return 1;
             }
             if (derivedClass.F2() != 30)
+            {
+                return 1;
+            }
+
+            if (derivedClass.F3(1, 2, 3) != 7)
             {
                 return 1;
             }
