f8657bc59e7181d4a38bfaf5c7014d1e8334c00d
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2024-09-17 22:47:29 +0100

    Add collecion/list interfaces

---
+++ System.Private.CoreLib/System/Collections/IList.cs
@@ -0,0 +1,21 @@
+﻿namespace System.Collections
+{
+    public interface IList : ICollection
+    {
+        object? this[int index] { get; set; }
+
+        int Add(object? value);
+
+        bool Contains(object? value);
+
+        void Clear();
+
+        int IndexOf(object? value);
+
+        void Insert(int index, object? value);
+
+        void Remove(object? value);
+
+        void RemoveAt(int index);
+    }
+}
