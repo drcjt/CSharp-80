a8773338ba0e831d4f608164eb4c31f5560bd93d
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2024-09-25 20:25:43 +0100

    Add code gen tests to cover signed integer division by cosntants

---
+++ Tests/CodeGen/DivConst/DivConst.cs
@@ -0,0 +1,50 @@
+﻿namespace DivConst
+{
+    static class DivConst
+    {
+        public static int I4_Div_1(int i4) => i4 / 1;
+        public static int I4_Div_Minus1(int i4) => i4 / -1;
+        public static int I4_DivPow2_2(int i4) => i4 / 2;
+    }
+
+    public static class Program
+    {
+        public static int Main()
+        {
+            const int Pass = 0;
+            const int Fail = -1;
+
+            if (DivConst.I4_Div_1(42) != 42)
+            {
+                return Fail;
+            }
+
+            if (DivConst.I4_Div_Minus1(42) != -42)
+            {
+                return Fail;
+            }
+
+            if (DivConst.I4_DivPow2_2(42) != 21)
+            {
+                return Fail;
+            }
+
+            if (DivConst.I4_DivPow2_2(43) != 21)
+            {
+                return Fail;
+            }
+
+            if (DivConst.I4_DivPow2_2(-42) != -21)
+            {
+                return Fail;
+            }
+
+            if (DivConst.I4_DivPow2_2(-43) != -21)
+            {
+                return Fail;
+            }
+
+            return Pass;
+        }
+    }
+}
