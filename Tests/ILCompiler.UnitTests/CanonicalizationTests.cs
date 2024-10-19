650c989ad9073530c44392c462e0eda3e015ee95
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2024-10-19 22:36:36 +0100

    Some initial canonicalization tests

---
+++ Tests/ILCompiler.UnitTests/CanonicalizationTests.cs
@@ -0,0 +1,79 @@
+﻿using dnlib.DotNet;
+using ILCompiler.Compiler;
+using ILCompiler.TypeSystem.Canon;
+using ILCompiler.TypeSystem.Common;
+using ILCompiler.TypeSystem.Dnlib;
+using NUnit.Framework;
+using System.IO;
+using System.Reflection;
+
+namespace ILCompiler.UnitTests
+{
+    internal class CanonicalizationTests
+    {
+        private ModuleDefMD _testModule;
+        private DnlibModule _module;
+
+        private MetadataType _referenceType;
+        private MetadataType _otherReferenceType;
+        private MetadataType _genericReferenceType;
+
+        private readonly string SolutionPath = Path.Combine(TestContext.CurrentContext.TestDirectory, @".\..\..\..\..\..");
+
+        [SetUp]
+        public void Setup()
+        {
+            ModuleContext modCtx = ModuleDef.CreateModuleContext();
+
+            var currentType = MethodBase.GetCurrentMethod().DeclaringType;
+            var assemblyConfigurationAttribute = currentType.Assembly.GetCustomAttribute<AssemblyConfigurationAttribute>();
+            var buildConfigurationName = assemblyConfigurationAttribute?.Configuration;
+
+            var corelibPath = Path.Combine(SolutionPath, $@".\System.Private.CoreLib\bin\Trs80\{buildConfigurationName}\net8.0\System.Private.CoreLib.dll");
+            ModuleDefMD corlibModule = ModuleDefMD.Load(corelibPath, modCtx);
+            ((AssemblyResolver)modCtx.AssemblyResolver).AddToCache(corlibModule);
+
+            var options = new ModuleCreationOptions(modCtx)
+            {
+                CorLibAssemblyRef = corlibModule.Assembly.ToAssemblyRef()
+            };
+            string inputFilePath = Path.Combine(SolutionPath, $@".\Tests\CoreTestAssembly\bin\{buildConfigurationName}\net8.0\CoreTestAssembly.dll");
+            _testModule = ModuleDefMD.Load(inputFilePath, options);
+
+            var typeSystemContext = new TypeSystemContext();
+            var corLibModuleProvider = new CorLibModuleProvider();
+            corLibModuleProvider.CorLibModule = corlibModule;
+            _module = new DnlibModule(typeSystemContext, corLibModuleProvider);
+
+            _referenceType = GetType("Canonicalization", "ReferenceType");
+            _otherReferenceType = GetType("Canonicalization", "OtherReferenceType");
+            _genericReferenceType = GetType("Canonicalization", "GenericReferenceType`1");
+        }
+
+        [Test]
+        public void TestGenericTypes()
+        {
+            var referenceOverReference = MakeInstantiatedType(_genericReferenceType, _referenceType);
+            var referenceOverOtherReference = MakeInstantiatedType(_genericReferenceType, _otherReferenceType);
+
+            Assert.That(referenceOverReference.ConvertToCanonForm(CanonicalFormKind.Specific), 
+                Is.EqualTo(referenceOverOtherReference.ConvertToCanonForm(CanonicalFormKind.Specific)));
+
+            var referenceOverReferenceOverReference = MakeInstantiatedType(_genericReferenceType, referenceOverReference);
+
+            Assert.That(referenceOverReference.ConvertToCanonForm(CanonicalFormKind.Specific),
+                Is.EqualTo(referenceOverReferenceOverReference.ConvertToCanonForm(CanonicalFormKind.Specific)));
+        }
+
+        public static InstantiatedType MakeInstantiatedType(MetadataType typeDef, params TypeDesc[] genericParameters)
+        {
+            return typeDef.Context.GetInstantiatedType(typeDef, new Instantiation(genericParameters));
+        }
+
+        public MetadataType GetType(string nameSpace, string name)
+        {
+            var typeDef = _testModule.Find($"{nameSpace}.{name}", false);
+            return (MetadataType)_module.Create(typeDef);
+        }
+    }
+}
