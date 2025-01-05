17cab3da6f54c01ce471dff14fc3896e1e1f81d8
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-01-05 13:47:45 +0000

    Create Common folder for EEType and associated constants and use in ILCompiler

---
+++ Common/Internal/Runtime/EEType.cs
@@ -0,0 +1,48 @@
+﻿using Internal.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+
+namespace Internal.Runtime
+{
+    internal unsafe struct EEType
+    {
+        // CS0649: Field '{blah}' is never assigned to, and will always have its default value
+#pragma warning disable 649
+        private ushort _usComponentSize;
+        private ushort _usFlags;
+        private ushort _usBaseSize;
+        private EEType* _relatedType;
+        private byte _numVtableSlots;
+        private byte _numInterfaces;
+#pragma warning restore
+
+        public readonly ushort GetFlags() { return _usFlags; }
+
+        internal readonly EEType* RelatedType => _relatedType;
+
+        [Intrinsic]
+        internal static extern EEType* Of<T>();
+
+        internal bool HasCctor
+        {
+            get
+            {
+                return _usFlags == 1;
+            }
+        }
+
+        internal byte NumVTableSlots => _numVtableSlots;
+        internal byte NumInterfaces => _numInterfaces;
+
+        internal EEType** InterfaceMap => (EEType**)((byte*)Unsafe.AsPointer(ref this) + sizeof(EEType) + sizeof(void*) * _numVtableSlots);
+
+        internal ushort ComponentSize => _usComponentSize;
+
+        internal ushort BaseSize => _usBaseSize;
+
+        internal bool IsValueType => ElementType < EETypeElementType.Class;
+
+        internal EETypeElementType ElementType => (EETypeElementType)((_usFlags & (ushort)EETypeFlags.ElementTypeMask) >> (byte)EETypeFlags.ElementTypeShift);
+
+        internal uint ValueTypeSize => (uint)(BaseSize - 2);
+    }
+}
---
+++ Common/Internal/Runtime/EETypeFlags.cs
@@ -0,0 +1,8 @@
+﻿namespace Internal.Runtime
+{
+    internal enum EETypeFlags : ushort
+    {
+        ElementTypeMask = 0x7C00,
+        ElementTypeShift = 26,
+    }
+}
