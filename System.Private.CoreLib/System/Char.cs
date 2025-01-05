fad2d59d970fcdcaf2dd32fc83a0ae6bddcac1dc
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-01-05 14:09:28 +0000

    Add proper Equals implementation to Char for comparing boxed chars

--- System.Private.CoreLib/System/Char.cs
+++ System.Private.CoreLib/System/Char.cs
@@ -2,8 +2,21 @@
 {
     public struct Char
     {
+        private readonly char m_value;
+
         public static bool IsBetween(char c, char minInclusive, char maxInclusive) => (uint)(c - minInclusive) <= (uint)(maxInclusive - minInclusive);
 
         public static bool IsAsciiDigit(char c) => IsBetween(c, '0', '9');
+
+        public static char ToLower(char c) => (char)(c | 32);
+        public static char ToUpper(char c) => (char)(c & ~32);
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not char)
+                return false;
+            return m_value == ((char)obj).m_value;
+        }
+
     }
 }
