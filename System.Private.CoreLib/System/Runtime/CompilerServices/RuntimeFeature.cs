caf2cbaf9a5febb68b63402a1d119d5438f809c8
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-01-09 21:07:54 +0000

    Enable NumericIntPtr feature flag (#565)
    Use nint and nuint in IntPtr and UIntPtr
    Enable array tests for indexof covering IntPtr and UIntPtr

--- System.Private.CoreLib/System/Runtime/CompilerServices/RuntimeFeature.cs
+++ System.Private.CoreLib/System/Runtime/CompilerServices/RuntimeFeature.cs
@@ -3,7 +3,7 @@
     public static class RuntimeFeature
     {
         public const string DefaultImplementationsOfInterfaces = nameof(DefaultImplementationsOfInterfaces);
-
+        public const string NumericIntPtr = nameof(NumericIntPtr);
         public static bool IsSupported(string feature)
         {
             return true;
