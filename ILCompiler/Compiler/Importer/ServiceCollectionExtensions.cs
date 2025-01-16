2c8e9542be5d38de1516eaa5ab399d830aa6219d
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-01-16 20:57:14 +0000

    Add ReadonlyImporter as placeholder

--- ILCompiler/Compiler/Importer/ServiceCollectionExtensions.cs
+++ ILCompiler/Compiler/Importer/ServiceCollectionExtensions.cs
@@ -6,6 +6,7 @@ namespace ILCompiler.Compiler.Importer
     {
         public static IServiceCollection AddImporters(this IServiceCollection services)
         {
+            services.AddSingleton<IOpcodeImporter, ReadonlyImporter>();
             services.AddSingleton<IOpcodeImporter, ConstrainedImporter>();
             services.AddSingleton<IOpcodeImporter, UnboxImporter>();
             services.AddSingleton<IOpcodeImporter, BoxImporter>();
