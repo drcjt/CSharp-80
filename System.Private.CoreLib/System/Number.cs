df102398940f22adac50d9020b697ec916d583b9
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-02-06 19:49:36 +0000

    Handle negative numbers correctly in Number.Int32ToDecStr (#580)

--- System.Private.CoreLib/System/Number.cs
+++ System.Private.CoreLib/System/Number.cs
@@ -7,39 +7,55 @@ namespace System
     {
         public unsafe static string Int32ToDecStr(int value)
         {
-            if (value == 0) return "0";
+            if (value >= 0)
+                return UInt32ToDecStr((uint)value);
+            else
+                return NegativeInt32ToDecStr(value);
+        }
 
-            bool isNegative = value < 0;
-            value = Math.Abs(value);
+        private static unsafe string UInt32ToDecStr(uint value)
+        {
+            if (value == 0)
+                return "0";
 
             int bufferLength = CountDigits(value);
 
-            if (isNegative)
+            string result = RuntimeImports.NewString(EEType.Of<string>(), bufferLength);
+            fixed (char* buffer = result)
             {
-                bufferLength++;
+                char* p = buffer + bufferLength;
+                UInt32ToDecStr(p, value);
+            }
+            return result;
+        }
+
+        private static unsafe char* UInt32ToDecStr(char* bufferEnd, uint value)
+        {
+            while (value != 0)
+            {
+                *(--bufferEnd) = (char)((value % 10) + '0');
+                value /= 10;
             }
 
+            return bufferEnd;
+        }
+        private static unsafe string NegativeInt32ToDecStr(int value)
+        {
+            int bufferLength = CountDigits((uint)-value) + 1;
             string result = RuntimeImports.NewString(EEType.Of<string>(), bufferLength);
 
             fixed (char* buffer = result)
             {
                 char* p = buffer + bufferLength;
-                while (value > 0)
-                {
-                    *(--p) = (char)((value % 10) + '0');
-                    value /= 10;
-                }
+                p = UInt32ToDecStr(p, (uint)-value);
 
-                if (isNegative)
-                {
-                    *(p-1) = '-';
-                }
+                *(p-1) = '-';
             }
 
             return result;
         }
 
-        private static int CountDigits(int value)
+        private static int CountDigits(uint value)
         {
             int count = 0;
 
