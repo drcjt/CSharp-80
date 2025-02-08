0105a8fb066cb59c1512c12beb64a026eeba6d54
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-02-08 17:22:05 +0000

    Fix problem with int32 comparisons not handling overflow properly. Add/improve tests for int32 comparisons

--- ILCompiler/Runtime/i_gt.asm
+++ ILCompiler/Runtime/i_gt.asm
@@ -11,16 +11,31 @@ i_gt:
 
 	POP HL
 
-	XOR A			; Clear carry flag
-	SBC HL, DE
-
-	EX DE, HL
+	LD A, L
+	SUB A, E
+	LD A, H
+	SBC A, D
 
 	POP HL
-	SBC HL, BC
 
+	LD A, L
+	SBC A, C
 	LD A, H
-	ADD A, A
+	SBC A, B
+
+	JP PO, $+5
+	XOR A, 0x80
+	JP M, i_gt_1
 
 	EXX
+
+	AND A
+
+	JP (HL)
+
+i_gt_1:
+	EXX
+
+	SCF
+
 	JP (HL)
