0ff37aeac0bc07f6f3d37cee6a383e5fbae454fc
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-02-10 22:00:50 +0000

    Implement EqualityComparer<T>.Create as intrinsic to determine appropriate comparer to use. Add GenericEqualityComparer<T>

--- ILCompiler/IL/Stubs/ComparerIntrinsics.cs
+++ ILCompiler/IL/Stubs/ComparerIntrinsics.cs
@@ -1,11 +1,58 @@
 ﻿using ILCompiler.TypeSystem.Canon;
 using ILCompiler.TypeSystem.Common;
+using ILCompiler.TypeSystem.IL;
 using System.Diagnostics;
 
 namespace ILCompiler.IL.Stubs
 {
     internal static class ComparerIntrinsics
     {
+        public static MethodIL? EmitEqualityComparer(MethodDesc method)
+        {
+            return EmitComparerAndEqualityComparerCreateCommon(method, "EqualityComparer", "IEquatable`1");
+        }
+
+        private static MethodIL? EmitComparerAndEqualityComparerCreateCommon(MethodDesc methodBeingGenerated, string flavor, string interfaceName)
+        {
+            var owningType = methodBeingGenerated.OwningType;
+            var comparedType = owningType.Instantiation![0];
+
+            if (comparedType.IsCanonicalSubtype(CanonicalFormKind.Any))
+                throw new NotImplementedException("No support for using type loader to load proper EqualityComparer");
+
+            var comparerType = GetComparerForType(comparedType, flavor, interfaceName);
+
+            var body = new MethodIL();
+
+            var comparerTypeCtor = comparerType.GetKnownMethod(".ctor");
+
+            body.Instructions.Add(new Instruction(ILOpcode.newobj, 0, comparerTypeCtor));
+            body.Instructions.Add(new Instruction(ILOpcode.ret, 1));
+
+            return body;
+        }
+
+        private static TypeDesc GetComparerForType(TypeDesc type, string flavor, string interfaceName)
+        {
+            var context = type.Context;
+
+            if (context.IsCanonicalDefinitionType(type, CanonicalFormKind.Any) || (type.IsRuntimeDeterminedSubtype && !type.HasInstantiation))
+            {
+                // Can't determine the exact type for the comparer at compile time.
+                throw new NotImplementedException("Runtime determined comparer types not supported");
+            }
+
+            bool? implementsInterfaceOfSelf = ImplementsInterfaceOfSelf(type, interfaceName);
+            if (implementsInterfaceOfSelf is null)
+            {
+                throw new ArgumentException($"Type {type.FullName} does not implement {interfaceName}");
+            }
+
+            var comparerTypeName = (bool)implementsInterfaceOfSelf ? $"Generic{flavor}`1" : $"Object{flavor}`1";
+            return context.SystemModule!.GetKnownType("System.Collections.Generic", comparerTypeName).MakeInstantiatedType(type);
+        }
+
+
         public static bool? ImplementsIEquatable(TypeDesc type) => ImplementsInterfaceOfSelf(type, "IEquatable`1");
 
         private static bool? ImplementsInterfaceOfSelf(TypeDesc type, string interfaceName)
--- System.Private.CoreLib/System/Collections/Generic/EqualityComparer.cs
+++ System.Private.CoreLib/System/Collections/Generic/EqualityComparer.cs
@@ -4,12 +4,10 @@ namespace System.Collections.Generic
 {
     public abstract class EqualityComparer<T> : IEqualityComparer, IEqualityComparer<T>
     {
-        // TODO: Use Intrinsic to pick appropriate comparer here
         [Intrinsic]
         private static EqualityComparer<T> Create()
         {
-            Console.WriteLine("Creating ObjectEqualityComparer");
-            return new ObjectEqualityComparer<T>();
+            throw new Exception();
         }
 
         public static EqualityComparer<T> Default { get; } = Create();
@@ -32,6 +30,32 @@ namespace System.Collections.Generic
     }
 
     public sealed class ObjectEqualityComparer<T> : EqualityComparer<T>
+    {
+        public override bool Equals(T x, T y)
+        {
+            if (x is not null)
+            {
+                if (y is not null) return x.Equals(y);
+                return false;
+            }
+            if (y is not null) return false;
+            return true;
+        }
+
+        public override bool Equals(object? obj) => throw new NotImplementedException();
+
+        public override int GetHashCode(T obj)
+        {
+            if (obj is not null)
+                return obj.GetHashCode();
+
+            return 0;
+        }
+
+        public override int GetHashCode() => throw new NotImplementedException();
+    }
+
+    public sealed class GenericEqualityComparer<T> : EqualityComparer<T> where T : IEquatable<T>
     {
         public override bool Equals(T x, T y)
         {
