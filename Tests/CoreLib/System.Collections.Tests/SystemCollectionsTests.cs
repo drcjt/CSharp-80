b6f1edb7d32bc6505df642a70e8cbea7048a26cb
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-02-11 18:51:27 +0000

    Add EqualityComparer tests

---
+++ Tests/CoreLib/System.Collections.Tests/SystemCollectionsTests.cs
@@ -0,0 +1,13 @@
+﻿namespace System.Collections.Tests
+{
+    public static class SystemCollectionsTests
+    {
+        public static int Main()
+        {
+            EqualityComparerTests.Default_ForType_CreatesNoMoreThanOneComparerInstance();
+            EqualityComparerTests.EqualsTests();
+
+            return 0;
+        }
+    }
+}
