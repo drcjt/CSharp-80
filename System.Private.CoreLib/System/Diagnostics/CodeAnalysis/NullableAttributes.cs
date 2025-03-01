34ee90cadb500fbac5075be9282844a8ac16c2cf
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-03-01 21:34:23 +0000

    Add DrawPolygon method to Graphics class (#597)
    Add Point struct
    Add NonNullWhen attribute
    Simplify some code in DrawLine

--- System.Private.CoreLib/System/Diagnostics/CodeAnalysis/NullableAttributes.cs
+++ System.Private.CoreLib/System/Diagnostics/CodeAnalysis/NullableAttributes.cs
@@ -1,10 +1,15 @@
-﻿using System;
-
-namespace System.Diagnostics.CodeAnalysis
+﻿namespace System.Diagnostics.CodeAnalysis
 {
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    public sealed class NotNullWhenAttribute(bool returnValue) : Attribute
+    {
+        public bool ReturnValue { get; } = returnValue;
+    }
+
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
     public sealed class MemberNotNullAttribute : Attribute 
     {
-        public MemberNotNullAttribute(string member) => Members = new[] { member };
+        public MemberNotNullAttribute(string member) => Members = [member];
 
         public MemberNotNullAttribute(params string[] members) => Members = members;
 
