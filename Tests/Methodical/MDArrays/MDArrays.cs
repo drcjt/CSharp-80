0d08dd7ed4a118048dbb37b7bdee0c28d0ae3543
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-03-19 21:29:21 +0000

    Add simple test for multidimensional arrays

---
+++ Tests/Methodical/MDArrays/MDArrays.cs
@@ -0,0 +1,39 @@
+﻿namespace MDArrays
+{
+    public static class Test
+    {
+        public static int Main()
+        {
+            int result = Simple_MDArray_Test();
+            return result;
+        }
+
+        private static int Simple_MDArray_Test()
+        {
+            int[,] array = new int[3, 5];
+
+            int value = 1;
+            for (int x = 0; x < array.GetLength(0); x++)
+            {
+                for (int y = 0; y < array.GetLength(1); y++)
+                {
+                    array[x, y] = value++;
+                }
+            }
+
+            value = 1;
+            for (int x = 0; x < array.GetLength(0); x++)
+            {
+                for (int y = 0; y < array.GetLength(1); y++)
+                {
+                    if (array[x, y] != value++)
+                    {
+                        return 1;
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
