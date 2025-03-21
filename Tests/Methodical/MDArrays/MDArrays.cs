c8545606d4d1ce460dc8479319f0ad92c2d6fe22
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-03-21 20:25:26 +0000

    Extend InitializeArray intrinsic implementation to work with multi-dimensional arrays

--- Tests/Methodical/MDArrays/MDArrays.cs
+++ Tests/Methodical/MDArrays/MDArrays.cs
@@ -4,8 +4,28 @@
     {
         public static int Main()
         {
-            int result = Simple_MDArray_Test();
+            int result = Simple_MDArray_Test(); if (result != 0) return result;
+            result = InitializeArray_Test(); if (result != 0) return result;
+
             return result;
+        }
+
+        private static int InitializeArray_Test()
+        {
+            var array = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+            int value = 1;
+            for (int x = 0; x < array.GetLength(0); x++)
+            {
+                for (int y = 0; y < array.GetLength(1); y++)
+                {
+                    if (array[x, y] != value++)
+                    {
+                        return 1;
+                    }
+                }
+            }
+
+            return 0;
         }
 
         private static int Simple_MDArray_Test()
