753bff65de59025972e7d95cebc8f71e159c0a68
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-04-01 20:56:16 +0100

    Add some tests for generic multi dimensional arrays (#605)

---
+++ Tests/Generics/Arrays/ConstructedTypes/MultiDim/TestClass.cs
@@ -0,0 +1,55 @@
+﻿namespace MultiDim.ConstructedTypes
+{
+    public class GenericWrapper<T>
+    {
+        public T Field;
+
+        public GenericWrapper(T field)
+        {
+            Field = field;
+        }
+    }
+
+    public static class TestClass
+    {
+        public static int Main()
+        {
+            int size = 10;
+            int i, j, k;
+            int locationNumber = 0;
+
+            GenericWrapper<int>[,,] GenArray = new GenericWrapper<int>[size, size, size];
+            for (i = 0; (i < size); i++)
+            {
+                for (j = 0; (j < size); j++)
+                {
+                    for (k = 0; (k < size); k++)
+                    {
+                        GenArray[i, j, k] = new GenericWrapper<int>(locationNumber);
+                        locationNumber++;
+                    }
+                }
+            }
+
+            int sum = 0;
+            for (i = 0; (i < size); i++)
+            {
+                for (j = 0; (j < size); j++)
+                {
+                    for (k = 0; (k < size); k++)
+                    {
+                        sum += GenArray[i, j, k].Field;
+                        locationNumber++;
+                    }
+                }
+            }
+
+            if (sum != 499500)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
