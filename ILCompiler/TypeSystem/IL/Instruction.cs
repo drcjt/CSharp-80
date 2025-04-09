d6148fc5fde2ebb31d232968599061afc1c175b5
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-04-09 23:11:46 +0100

    Add ILEmitter Add Locals to MethodIL Allow Instruction operand/offset to be changed

--- ILCompiler/TypeSystem/IL/Instruction.cs
+++ ILCompiler/TypeSystem/IL/Instruction.cs
@@ -3,10 +3,10 @@
     public class Instruction
     {
         public virtual ILOpcode Opcode { get; }
-        public virtual uint Offset { get; }
+        public virtual uint Offset { get; set; }
         public virtual int GetSize() => 1;
 
-        private readonly object? _operand = null;
+        private object? _operand = null;
         public virtual object Operand
         {
             get
@@ -17,12 +17,26 @@
                 }
                 return _operand;
             }
+            set
+            {
+                if (value is null)
+                {
+                    throw new ArgumentNullException(nameof(value), "Operand cannot be null");
+                }
+                _operand = value;
+            }
         }
 
         public Instruction(ILOpcode opcode, uint offset, object? operand = null)
         {
             Opcode = opcode;
             Offset = offset;
+            _operand = operand;
+        }
+
+        public Instruction(ILOpcode opcode, object? operand = null)
+        {
+            Opcode = opcode;
             _operand = operand;
         }
     }
