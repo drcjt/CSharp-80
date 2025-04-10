15199562e747b5bebba1a30d6bb977deaed7cff8
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-04-10 21:16:05 +0100

    Improve range check test for MD arrays to cover writes as well as reads from the array

--- Tests/Methodical/MDArrays/RangeCheck/RangeCheck.cs
+++ Tests/Methodical/MDArrays/RangeCheck/RangeCheck.cs
@@ -6,13 +6,15 @@ namespace RangeCheck
     {
         public static int Main()
         {
-            int result = IndexOutOfRangeException_Test_FirstDimension(); if (result != 0) return result;
-            result = IndexOutOfRangeException_Test_SecondDimension(); if (result != 0) return result;
+            int result = ReadBeyondEndOfFirstDimension_Test(); if (result != 0) return result;
+            result = ReadBeyondEndOfSecondDimension_Test(); if (result != 0) return result;
+            result = WriteBeyondEndOfFirstDimension_Test(); if (result != 0) return result;
+            result = WriteBeyondEndOfSecondDimension_Test(); if (result != 0) return result;
 
             return result;
         }
 
-        private static int IndexOutOfRangeException_Test_FirstDimension()
+        private static int ReadBeyondEndOfFirstDimension_Test()
         {
             int[,] array = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
             try
@@ -26,12 +28,40 @@ namespace RangeCheck
             }
         }
 
-        private static int IndexOutOfRangeException_Test_SecondDimension()
+        private static int ReadBeyondEndOfSecondDimension_Test()
         {
             int[,] array = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
             try
             {
                 int value = array[0, 3]; // This should throw an IndexOutOfRangeException
+                return 1; // If we reach here, the test failed
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return 0; // Test passed
+            }
+        }
+
+        private static int WriteBeyondEndOfFirstDimension_Test()
+        {
+            int[,] array = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+            try
+            {
+                array[2, 0] = 10; // This should throw an IndexOutOfRangeException
+                return 1; // If we reach here, the test failed
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return 0; // Test passed
+            }
+        }
+
+        private static int WriteBeyondEndOfSecondDimension_Test()
+        {
+            int[,] array = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+            try
+            {
+                array[0, 3] = 10; // This should throw an IndexOutOfRangeException
                 return 1; // If we reach here, the test failed
             }
             catch (IndexOutOfRangeException)
