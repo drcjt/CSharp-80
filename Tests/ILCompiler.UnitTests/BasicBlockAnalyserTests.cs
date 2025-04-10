89781cd4fd96041802a871d0f63beddf72de992f
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-04-10 21:22:38 +0100

    Fix unit tests

--- Tests/ILCompiler.UnitTests/BasicBlockAnalyserTests.cs
+++ Tests/ILCompiler.UnitTests/BasicBlockAnalyserTests.cs
@@ -24,10 +24,18 @@ namespace ILCompiler.Tests
             return module.Create(methodDef);
         }
 
+        private DnlibModule CreateModule()
+        {
+            return new DnlibModule(
+                new TypeSystemContext(new Configuration()), 
+                new Compiler.CorLibModuleProvider(), 
+                new RTILProvider());
+        }
+
         [Test]
         public void FindBasicBlocks_WithNoBranches_CreatesBasicBlockWithAlwaysJumpKind()
         {
-            var module = new DnlibModule(new TypeSystemContext(), new Compiler.CorLibModuleProvider(), new RTILProvider());
+            var module = CreateModule();
             var method = BuildMethod(new List<Instruction>()
             {
                 new Instruction(OpCodes.Ldc_I4_0),
@@ -45,7 +53,7 @@ namespace ILCompiler.Tests
         [Test]
         public void FindBasicBlocks_WithOnlyReturn_CreatesBasicBlockWithReturnJumpKind()
         {
-            var module = new DnlibModule(new TypeSystemContext(), new Compiler.CorLibModuleProvider(), new RTILProvider());
+            var module = CreateModule();
             var method = BuildMethod(new List<Instruction>()
             {
                 new Instruction(OpCodes.Ret),
@@ -62,7 +70,7 @@ namespace ILCompiler.Tests
         [Test]
         public void FindBasicBlocks_WithSwitch_CreatesBasicBlockWithSwitchJumpKind()
         {
-            var module = new DnlibModule(new TypeSystemContext(), new Compiler.CorLibModuleProvider(), new RTILProvider());
+            var module = CreateModule();
             var method = BuildMethod(new List<Instruction>()
             {
                 new Instruction(OpCodes.Switch, System.Array.Empty<Instruction>()),
@@ -86,7 +94,7 @@ namespace ILCompiler.Tests
             code.Add(OpCodes.Brfalse.ToInstruction(branchTarget));
             code.Add(OpCodes.Nop.ToInstruction());
             code.Add(branchTarget);
-            var module = new DnlibModule(new TypeSystemContext(), new Compiler.CorLibModuleProvider(), new RTILProvider());
+            var module = CreateModule();
             var method = BuildMethod(code, module);
             var basicBlockAnalyser = new BasicBlockAnalyser(method);
             var offsetToIndexMap = new Dictionary<int, int>();
@@ -100,7 +108,7 @@ namespace ILCompiler.Tests
         [Test]
         public void FindBasicBlocks_WithNoBranches_IdentifiesSingleBasicBlock()
         {
-            var module = new DnlibModule(new TypeSystemContext(), new Compiler.CorLibModuleProvider(), new RTILProvider());
+            var module = CreateModule();
             var method = BuildMethod(new List<Instruction>()
             {
                 new Instruction(OpCodes.Ldc_I4_0),
@@ -125,7 +133,7 @@ namespace ILCompiler.Tests
             code.Add(OpCodes.Brfalse.ToInstruction(branchTarget));
             code.Add(OpCodes.Nop.ToInstruction());
             code.Add(branchTarget);
-            var module = new DnlibModule(new TypeSystemContext(), new Compiler.CorLibModuleProvider(), new RTILProvider());
+            var module = CreateModule();
             var method = BuildMethod(code, module);
             var basicBlockAnalyser = new BasicBlockAnalyser(method);
             var offsetToIndexMap = new Dictionary<int, int>();
@@ -147,7 +155,7 @@ namespace ILCompiler.Tests
             var instructionAfterBranch = OpCodes.Nop.ToInstruction();
             code.Add(instructionAfterBranch);
             code.Add(branchTarget);
-            var module = new DnlibModule(new TypeSystemContext(), new Compiler.CorLibModuleProvider(), new RTILProvider());
+            var module = CreateModule();
             var method = BuildMethod(code, module);
             var basicBlockAnalyser = new BasicBlockAnalyser(method);
             var offsetToIndexMap = new Dictionary<int, int>();
@@ -167,7 +175,7 @@ namespace ILCompiler.Tests
             code.Add(OpCodes.Nop.ToInstruction());
             code.Add(branchTarget);
             code.Add(OpCodes.Br.ToInstruction(branchTarget));
-            var module = new DnlibModule(new TypeSystemContext(), new Compiler.CorLibModuleProvider(), new RTILProvider());
+            var module = CreateModule();
             var method = BuildMethod(code, module);
             var basicBlockAnalyser = new BasicBlockAnalyser(method);
             var offsetToIndexMap = new Dictionary<int, int>();
