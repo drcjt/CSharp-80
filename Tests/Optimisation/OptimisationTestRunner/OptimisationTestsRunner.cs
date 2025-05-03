239e93f4ed4054978b54aad37291dfd424cb791d
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-05-03 17:03:20 +0100

    Add simple test for early value propagation of constant array size to ldlen

---
+++ Tests/Optimisation/OptimisationTestRunner/OptimisationTestsRunner.cs
@@ -0,0 +1,19 @@
+﻿using ILCompiler.Tests.Common;
+using NUnit.Framework;
+
+namespace OptimisationTests
+{
+    [TestFixture]
+    public class OptimisationTestsRunner
+    {
+        [Test]
+        [TestCaseSource(typeof(OptimisationTestsRunner), nameof(OptimisationTestsCaseData))]
+        public void OptimisationTest(string testName)
+        {
+            TestRunnerHelper.RunTest(testName, @".\..\..\..\..\..\..");
+            Assert.Pass();
+        }
+
+        private static IEnumerable<TestCaseData> OptimisationTestsCaseData => TestRunnerHelper.TestsCaseData();
+    }
+}
