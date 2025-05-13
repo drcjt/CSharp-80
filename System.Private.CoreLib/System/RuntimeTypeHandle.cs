3c20b8220eac3d45a2fcfd49fb42c2d755f63c69
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-05-13 23:24:31 +0100

    Add missing methods to RuntimeTypeHandle

--- System.Private.CoreLib/System/RuntimeTypeHandle.cs
+++ System.Private.CoreLib/System/RuntimeTypeHandle.cs
@@ -11,7 +11,17 @@
 
         internal static unsafe IntPtr GetValueInternal(RuntimeTypeHandle handle) => handle._EEType;
 
+        public override bool Equals(object? obj)
+        {
+            if (obj is not RuntimeTypeHandle)
+                return false;
+
+            return _EEType == ((RuntimeTypeHandle)obj)._EEType;
+        }
+
         public bool Equals(RuntimeTypeHandle handle) => _EEType == handle._EEType;
+
+        public override int GetHashCode() => _EEType.GetHashCode();
 
         public override string ToString() => _EEType.ToString();
     }
