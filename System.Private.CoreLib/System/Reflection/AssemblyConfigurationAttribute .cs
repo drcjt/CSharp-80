6af3ebac27725273e8010a318f5181c351eba900
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-05-14 21:52:44 +0100

    Add all assembly attributes to fix S3904 SQ issues (#628)
    * Add missing assembly attributes to allow them to be set via csproj
    
    * Allow assembly version attributes to be generated to fix SQ S3904
    
    * Fix more projects to correctly gen assembly attributes from csproj files

---
+++ System.Private.CoreLib/System/Reflection/AssemblyConfigurationAttribute .cs
@@ -0,0 +1,9 @@
+﻿namespace System.Reflection
+{
+    [AttributeUsage(AttributeTargets.Assembly, Inherited = false)]
+    public sealed class AssemblyConfigurationAttribute : Attribute
+    {
+        public AssemblyConfigurationAttribute(string configuration) { Configuration = configuration; }
+        public string Configuration { get; }
+    }
+}
---
+++ System.Private.CoreLib/System/Reflection/AssemblyTitleAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace System.Reflection
+{
+    [AttributeUsage(AttributeTargets.Assembly, Inherited = false)]
+    public sealed class AssemblyTitleAttribute : Attribute
+    {
+        public AssemblyTitleAttribute(string title) { Title = title; }
+        public string Title { get; }
+    }
+}
