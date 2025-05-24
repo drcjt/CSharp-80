9708e78fa70129ca82a1ecf2b518c35bc88e1ded
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-05-24 15:48:36 +0100

    Add simple inlining tests

---
+++ Tests/Methodical/Inlining/Inlining.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Inlining
+{
+    public static class Test
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void InlineMethod2()
+        {
+            _result = 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void InlineMethod()
+        {
+            InlineMethod2();
+        }
+
+        private static int _result = 1;
+
+        public static int Main()
+        {
+            InlineMethod();
+
+            return _result;
+        }
+    }
+}
