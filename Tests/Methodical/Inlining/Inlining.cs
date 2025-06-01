6fb44f6c5bb482369c9a7439735e0aa2a924c83b
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-06-01 18:39:00 +0100

    Extend inliner tests to include methods with parameters and locals

--- Tests/Methodical/Inlining/Inlining.cs
+++ Tests/Methodical/Inlining/Inlining.cs
@@ -4,6 +4,21 @@ namespace Inlining
 {
     public static class Test
     {
+        private static int _parameter = 0;
+        
+        // This is not initialized as this causes all od the static method
+        // calls to be imported as comma nodes to call the cctor before
+        // the method. 
+        private static string _str;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void InlineMethodWithParameters(int i, string s)
+        {
+            int j = i + 1;
+            _parameter = j;
+            _str = s;
+        }
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void InlineMethod2()
         {
@@ -21,6 +36,12 @@ namespace Inlining
         public static int Main()
         {
             InlineMethod();
+
+            string strParameter = "Test";
+            InlineMethodWithParameters(1, strParameter);
+
+            if (_parameter != 2) return 2;
+            if (!ReferenceEquals(_str, strParameter)) return 3;
 
             return _result;
         }
