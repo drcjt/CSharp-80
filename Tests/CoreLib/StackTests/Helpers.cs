f3bd09eb6d142736bf9c7af9c941deeaf2748ec0
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-07-16 19:53:37 +0100

    Add Stack collection class and some tests

---
+++ Tests/CoreLib/StackTests/Helpers.cs
@@ -0,0 +1,17 @@
+﻿namespace System.Collections.Tests
+{
+    internal static class Helpers
+    {
+        public static Stack CreateIntStack(int count, int start = 0)
+        {
+            var stack = new Stack(100);
+
+            for (int i = start; i < start + count; i++)
+            {
+                stack.Push(i);
+            }
+
+            return stack;
+        }
+    }
+}
