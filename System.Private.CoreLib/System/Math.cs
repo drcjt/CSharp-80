541333c876f3e16d442811b1762f9f88c01b66ae
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-07-30 21:50:26 +0100

    Add Math.Clamp

--- System.Private.CoreLib/System/Math.cs
+++ System.Private.CoreLib/System/Math.cs
@@ -29,5 +29,19 @@
         {
             return (val1 <= val2) ? val1 : val2;
         }
+
+        public static int Clamp(int value, int min, int max)
+        {
+            if (value < min)
+            {
+                return min;
+            }
+            else if (value > max)
+            {
+                return max;
+            }
+
+            return value;
+        }
     }
 }
