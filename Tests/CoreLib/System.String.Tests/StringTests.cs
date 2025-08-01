0e285b4432a4e87acefc00ff5499d9a5e73b4e94
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-08-01 19:58:36 +0100

    Use Xunit source generator for System.String tests

--- Tests/CoreLib/System.String.Tests/StringTests.cs
+++ Tests/CoreLib/System.String.Tests/StringTests.cs
@@ -5,47 +5,39 @@ namespace System.String.Tests
 {
     internal static class StringTests
     {
-        public static void Ctor_CharSpan_EmptyString_Tests()
-        {
-            Ctor_CharSpan_EmptyString(0, 0);
-            Ctor_CharSpan_EmptyString(3, 1);
-        }
-
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(3, 1)]
         public static void Ctor_CharSpan_EmptyString(int length, int offset)
         {
             var span = new ReadOnlySpan<char>(new char[length], offset, 0);
             Assert.Same(string.Empty, new string(span));
         }
 
+        [Fact]
         public static void Ctor_CharSpan_Empty()
         {
             Assert.Same(string.Empty, new string((ReadOnlySpan<char>)null));
             Assert.Same(string.Empty, new string(ReadOnlySpan<char>.Empty));
         }
 
-        public static void Ctor_CharSpan_Tests()
-        {
-            Ctor_CharSpan(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'], 0, 8, "abcdefgh");
-            Ctor_CharSpan(['a', 'b', 'c'], 0, 1, "a");
-            Ctor_CharSpan(['a', 'b', 'c'], 2, 1, "c");
-        }
-
-        private static void Ctor_CharSpan(char[] data, int startIndex, int length, string expected)
+        [Theory]
+        [InlineData(new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' }, 0, 8, "abcdefgh")]
+        [InlineData(new char[] { 'a', 'b', 'c' }, 0, 1, "a")]
+        [InlineData(new char[] { 'a', 'b', 'c' }, 2, 1, "c")]
+        public static void Ctor_CharSpan(char[] data, int startIndex, int length, string expected)
         {
             var span = new ReadOnlySpan<char>(data, startIndex, length);
             Assert.Equal(expected, new string(span));
         }
 
-        public static void Contains_Char_Tests()
-        {
-            Contains_Char("Hello", 'H', true);
-            Contains_Char("Hello", 'Z', false);
-            Contains_Char("Hello", 'e', true);
-            Contains_Char("Hello", 'E', false);
-            Contains_Char("", 'H', false);
-        }
-
-        private static void Contains_Char(string s, char value, bool expected)
+        [Theory]
+        [InlineData("Hello", 'H', true)]
+        [InlineData("Hello", 'Z', false)]
+        [InlineData("Hello", 'e', true)]
+        [InlineData("Hello", 'E', false)]
+        [InlineData("", 'H', false)]
+        public static void Contains_Char(string s, char value, bool expected)
         {
             Assert.Equal(expected, s.Contains(value));
 
@@ -53,6 +45,7 @@ namespace System.String.Tests
             Assert.Equal(expected, span.Contains(value));
         }
 
+        [Fact]
         public static void EqualsTests()
         {
             Assert.True("Hello".Equals("Hello"));
@@ -61,6 +54,7 @@ namespace System.String.Tests
             Assert.False("Hello".Equals((object)"World"));
         }
 
+        [Fact]
         public static void Contains_Match_Char()
         {
             Assert.False("".Contains('a'));
@@ -98,6 +92,7 @@ namespace System.String.Tests
             }
         }
 
+        [Fact]
         public static void Contains_ZeroLength_Char()
         {
             var span = new Span<char>(Array.Empty<char>());
@@ -120,6 +115,7 @@ namespace System.String.Tests
             Assert.False(found);
         }
 
+        [Fact]
         public static void Contains_MultipleMatches_Char()
         {
             for (int length = 2; length < 32; length++)
@@ -150,14 +146,11 @@ namespace System.String.Tests
             }
         }
 
-        public static void ImplicitCast_ResultingSpanMatches_Tests()
-        {
-            ImplicitCast_ResultingSpanMatches("");
-            ImplicitCast_ResultingSpanMatches("a");
-            ImplicitCast_ResultingSpanMatches("\0");
-            ImplicitCast_ResultingSpanMatches("abc");
-        }
-
+        [Theory]
+        [InlineData("")]
+        [InlineData("a")]
+        [InlineData("\0")]
+        [InlineData("abc")]
         public static unsafe void ImplicitCast_ResultingSpanMatches(string s)
         {
             ReadOnlySpan<char> span = s;
@@ -169,6 +162,7 @@ namespace System.String.Tests
             }
         }
 
+        [Fact]
         public static void ImplicitCast_NullString_ReturnsDefaultSpan()
         {
             ReadOnlySpan<char> span = (string)null;
