65c6ed009af8cfa556feaf1db5f185711e2bcaf5
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-08-01 19:13:13 +0100

    Add attributes for Xunit tests

---
+++ Tests/Common/FactAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Xunit
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class FactAttribute : Attribute
+    {
+
+    }
+}
---
+++ Tests/Common/InlineDataAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xunit
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class InlineDataAttribute : Attribute
+    {
+        public InlineDataAttribute(params object?[] dataValues)
+        {
+        }
+    }
+}
