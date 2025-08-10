648b9c322feabfa9b4661dd907d1272a6def21e5
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-08-10 20:10:41 +0100

    Add exception for invalid cast

---
+++ System.Private.CoreLib/System/InvalidCastException.cs
@@ -0,0 +1,14 @@
+﻿namespace System
+{
+    public sealed class InvalidCastException : Exception
+    {
+        const string DefaultMessage = "Specified cast is not valid.";
+        public InvalidCastException() : base(DefaultMessage)
+        { 
+        }
+
+        public InvalidCastException(string message) : base(message ?? DefaultMessage)
+        {
+        }
+    }
+}
