afa9c8c0843b69ecba2d1efe1af1631d57283908
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-08-29 22:10:43 +0100

    More CompareTo implementations

--- Tests/CoreLib/System.Tests/DateTimeTests.cs
+++ Tests/CoreLib/System.Tests/DateTimeTests.cs
@@ -4,12 +4,14 @@ namespace System.Tests
 {
     internal static class DateTimeTests
     {
-        private static readonly DateTime _testDateTime = new DateTime(10, 9, 8, 7);
+        private static readonly DateTime _testDateTime9 = new(9, 8, 7, 6);
+        private static readonly DateTime _testDateTime10 = new(10, 9, 8, 7);
+        private static readonly DateTime _testDateTime11 = new(11, 10, 9, 8);
 
         [Fact]
         public static void Ctor_Int_Int_Int_Int()
         {
-            VerifyDateTime(_testDateTime, 10, 9, 8, 7);
+            VerifyDateTime(_testDateTime10, 10, 9, 8, 7);
         }
 
         private static void VerifyDateTime(DateTime dateTime, int day, int hour, int minute, int second)
@@ -23,11 +25,11 @@ namespace System.Tests
         [Fact]
         public static void Equals_Tests()
         {
-            EqualsTest(_testDateTime, _testDateTime, true);
-            EqualsTest(_testDateTime, new DateTime(11, 10, 9, 8), false);
-            EqualsTest(_testDateTime, new DateTime(9, 8, 7, 6), false);
-            EqualsTest(_testDateTime, new object(), false);
-            EqualsTest(_testDateTime, null, false);
+            EqualsTest(_testDateTime10, _testDateTime10, true);
+            EqualsTest(_testDateTime10, _testDateTime11, false);
+            EqualsTest(_testDateTime10, _testDateTime9, false);
+            EqualsTest(_testDateTime10, new object(), false);
+            EqualsTest(_testDateTime10, null, false);
         }
 
         public static void EqualsTest(DateTime date, object? other, bool expected)
@@ -42,6 +44,46 @@ namespace System.Tests
             }
 
             Assert.Equal(expected, date.Equals(other));
+        }
+
+        [Fact]
+        public static void CompareTo_Invoke_ReturnsExpected_Tests()
+        {
+            CompareTo_Invoke_ReturnsExpected(_testDateTime10, _testDateTime10, 0);
+            CompareTo_Invoke_ReturnsExpected(_testDateTime10, _testDateTime11, -1);
+            CompareTo_Invoke_ReturnsExpected(_testDateTime10, _testDateTime9, 1);
+            CompareTo_Invoke_ReturnsExpected(_testDateTime10, null, 1);
+        }
+
+        public static void CompareTo_Invoke_ReturnsExpected(DateTime date, object other, int expected)
+        {
+            if (other is DateTime otherDate)
+            {
+                Assert.Equal(expected, date.CompareTo(otherDate));
+                Assert.Equal(expected, DateTime.Compare(date, otherDate));
+
+                Assert.Equal(expected > 0, date > otherDate);
+                Assert.Equal(expected >= 0, date >= otherDate);
+                Assert.Equal(expected < 0, date < otherDate);
+                Assert.Equal(expected <= 0, date <= otherDate);
+            }
+
+            Assert.Equal(expected, date.CompareTo(other));
+        }
+
+        [Fact]
+        public static void CompareTo_NotDateTime_ThrowsArgumentException()
+        {
+            try
+            {
+                DateTime.Now.CompareTo(new object());
+            }
+            catch (ArgumentException)
+            {
+                return;
+            }
+
+            Assert.Fail("");
         }
     }
 }
