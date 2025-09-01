9bd6743fe57bf242542d558bd3f032b69b25ad10
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-09-01 16:36:58 +0100

    Add exception class

---
+++ System.Private.CoreLib/System/DivideByZeroException.cs
@@ -0,0 +1,8 @@
+﻿namespace System
+{
+    public class DivideByZeroException : ArithmeticException
+    {
+        public DivideByZeroException() { }
+        public DivideByZeroException(string message) : base(message) { }
+    }
+}
