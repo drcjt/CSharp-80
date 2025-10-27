056a4452f634fa7a98226e803bb6eca1852d8805
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2025-10-27 22:10:24 +0000

    Lift out common math ops to helper (#711)

---
+++ ILCompiler/Compiler/Helpers/MathHelpers.cs
@@ -0,0 +1,27 @@
+﻿namespace ILCompiler.Compiler.Helpers
+{
+    internal static class MathHelpers
+    {
+        public static int Gcd(int a, int b)
+        {
+            while (a != 0)
+            {
+                (a, b) = (b % a, a);
+            }
+
+            return b;
+        }
+
+        public static bool IsPow2(int i) => (i > 0 && ((i - 1) & i) == 0);
+
+        public static int GetLog2(int i)
+        {
+            int r = 0;
+            while ((i >>= 1) != 0)
+            {
+                ++r;
+            }
+            return r;
+        }
+    }
+}
