383bb152f53b4ea430e2e8b67de118b2db821174
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2026-01-11 19:40:12 +0000

    Add custom dotnet template for creating CSharp-80 applications

---
+++ Templates/templates/template/Hello.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Hello
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
