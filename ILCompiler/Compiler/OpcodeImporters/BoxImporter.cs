88fe93d316246120bd97bbd7d54b515234e3867f
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2026-01-25 20:56:19 +0000

    Call the box runtime helper when importing a box opcode

--- ILCompiler/Compiler/OpcodeImporters/BoxImporter.cs
+++ ILCompiler/Compiler/OpcodeImporters/BoxImporter.cs
@@ -1,4 +1,5 @@
 ﻿using ILCompiler.Compiler.EvaluationStack;
+using ILCompiler.IL;
 using ILCompiler.Interfaces;
 using ILCompiler.TypeSystem.Common;
 using ILCompiler.TypeSystem.IL;
@@ -25,31 +26,20 @@ namespace ILCompiler.Compiler.OpcodeImporters
 
         public static StackEntry BoxValue(StackEntry value, TypeDesc objType, IImporter importer)
         {
-            // TODO: Consider creating a bespoke Box assembly code runtime routine to do the below
-            // Note - nativeaot implements this in C# in System.Runtime.RuntimeExports.RhBox
-
-            var lclNum = importer.GrabTemp(VarType.Ref, 2);
-
-            var mangledEETypeName = importer.NameMangler.GetMangledTypeName(objType);
+            string mangledEETypeName = importer.NameMangler.GetMangledTypeName(objType);
             var eeTypeNode = new NativeIntConstantEntry(mangledEETypeName);
 
-            int unboxedObjectSize = objType.GetElementSize().AsInt;
-
-            // Allocate memory for object
-            var op1 = new AllocObjEntry(eeTypeNode, VarType.Ref);
-            var asg = new StoreLocalVariableEntry(lclNum, false, op1);
+            // Spill value into a local so we can take its address to pass to the runtime helper
+            // TODO: Optimize this to avoid the local if possible
+            var lclNum = importer.GrabTemp(value.Type, value.ExactSize);
+            var asg = new StoreLocalVariableEntry(lclNum, false, value);
             importer.ImportAppendTree(asg);
 
-            // Copy value type into box
-            var newObjThisPtr = new LocalVariableEntry(lclNum, VarType.Ref, 2);
-            var headerOffset = new NativeIntConstantEntry(2);
-            var addr = new BinaryOperator(Operation.Add, isComparison: false, newObjThisPtr, headerOffset, VarType.Ptr);
+            var arguments = new List<StackEntry> { eeTypeNode, new LocalVariableAddressEntry(lclNum) };
+            MethodDesc runtimeHelperMethod = importer.Method.Context.GetCoreLibEntryPoint("System.Runtime", "RuntimeExports", "Box");
+            string mangledHelperMethod = importer.NameMangler.GetMangledMethodName(runtimeHelperMethod);
+            var node = new CallEntry(mangledHelperMethod, arguments, VarType.Ref, null);
 
-            var op = new StoreIndEntry(addr, value, objType.VarType, 0, unboxedObjectSize);
-            importer.ImportAppendTree(op);
-
-            // Push temp
-            var node = new LocalVariableEntry(lclNum, VarType.Ref, 2);
             return node;
         }
     }
