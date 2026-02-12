be08d073f551fb09dce9e6ed8be393cb723155a1
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2026-02-12 21:49:13 +0000

    Initobj needs to spill (#755)
    * Initobj needs to spill
    Add regression test to validate initobj spilling
    
    * Add regression test project accidentally removed in last commit

---
+++ Tests/Regression/Bug_754/InitObjNotSpilling.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Xunit;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+namespace Regression
+{
+    public struct S
+    {
+        public int Value;
+    }
+
+    public static class InitObjNotSpilling
+    {
+        [Fact]
+        public static void BugXXXTest()
+        {
+            Assert.True(N(new S() { Value = 25 }, default(S)));
+        }
+
+        public static bool N(S x, S y) => x.Value != y.Value;
+    }
+}
