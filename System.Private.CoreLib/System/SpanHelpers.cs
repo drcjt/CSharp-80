1210a518ef3bd3dcb21860abb536fd1041f6ed55
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2026-03-06 22:39:48 +0000

    Add missing SpanHelpers.SequenceEqualByte used in ValueType

--- System.Private.CoreLib/System/SpanHelpers.cs
+++ System.Private.CoreLib/System/SpanHelpers.cs
@@ -35,6 +35,27 @@ namespace System
             }
         }
 
+        public static bool SequenceEqualByte(ref byte first, ref byte second, int length)
+        {
+            int index = 0;
+            byte lookUp0;
+            byte lookUp1;
+
+            while (length > 0)
+            {
+                lookUp0 = Unsafe.Add(ref first, index);
+                lookUp1 = Unsafe.Add(ref second, index);
+
+                if (!lookUp0.Equals(lookUp1))
+                    return false;
+                index += 1;
+                length--;
+            }
+
+            return true;
+        }
+
+
         public static bool SequenceEqual<T>(ref T first, ref T second, int length) where T : IEquatable<T>
         {
             int index = 0;
@@ -45,6 +66,7 @@ namespace System
             {
                 lookUp0 = Unsafe.Add(ref first, index);
                 lookUp1 = Unsafe.Add(ref second, index);
+
                 if (!lookUp0.Equals(lookUp1))
                     return false;
                 index += 1;
