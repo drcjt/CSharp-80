1ea91b992071bd8279dc139960097d483cc4a12b
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2026-03-29 15:42:42 +0100

    Add ActiveIssue attribute. (#763)
    XUnit source generator skips tests marked with this attribute directly or on containing symbol

---
+++ Tests/Common/ActiveIssueAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Xunit
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
+    public sealed class ActiveIssueAttribute : Attribute
+    {
+        public ActiveIssueAttribute(string issue) => Issue = issue;
+        public string Issue { get; }
+    }
+}
