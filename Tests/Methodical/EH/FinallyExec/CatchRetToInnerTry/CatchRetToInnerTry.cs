0418abeb4e65a5d4dcb81281b34704146522ba5d
Colin Taylor <5084481+drcjt@users.noreply.github.com>
2026-06-03 20:25:26 +0100

    Add yet more eh tests (#778)
    * Add more EH tests
    
    * Add test to cover exception being thrown from a filter

---
+++ Tests/Methodical/EH/FinallyExec/CatchRetToInnerTry/CatchRetToInnerTry.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace CatchRetToInnerTry
+{
+    internal static class Test
+    {
+        public static int Main()
+        {
+            int i = 3;
+
+            beginloop:
+
+            try
+            {
+                try
+                {
+                    if (i == 3)
+                    {
+                        throw new IndexOutOfRangeException();
+                    }
+                    else if (i == 4)
+                    {
+                        Log.Mark(2);    // bye
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log.Mark(1);    // Caught an exception
+                    i++;
+                    goto beginloop;
+                }
+            }
+            finally
+            {
+                Log.Mark(3);    // In outer finally
+            }
+
+            return AssertSequence(new[] { 1, 3, 2, 3 });
+        }
+
+        public static int AssertSequence(int[] expected)
+        {
+            if (Log.Index != expected.Length)
+                return 1;
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                if (Log.Events[i] != expected[i])
+                    return 1;
+            }
+
+            return 0;
+        }
+    }
+
+    public static class Log
+    {
+        public static int[] Events { get; } = new int[10];
+        public static int Index { get; set; }
+
+        public static void Mark(int id) => Events[Index++] = id;
+    }
+}
---
+++ Tests/Methodical/EH/FinallyExec/LocalGotoInAHandler/LocalGotoInAHandler.cs
@@ -0,0 +1,67 @@
+﻿namespace LocalGotoInAHandler
+{
+    internal static class Test
+    {
+        public static int Main()
+        {
+            try
+            {
+                Log.Mark(1);    // in main try
+            }
+            finally
+            {
+                Log.Mark(2);    // in main finally
+
+                try
+                {
+                    Log.Mark(3);    // in inner try 1
+
+                    try
+                    {
+                        Log.Mark(4);    // in inner try 2
+
+                        goto LABEL;
+                    }
+                    catch
+                    {
+                        Log.Mark(5);    // will never see this catch
+                    }
+
+                    Log.Mark(6);    // will never see this code, jumping over it
+
+                LABEL:
+
+                    Log.Mark(7);    // Back in inner try 1
+                }
+                finally
+                {
+                    Log.Mark(8);    // in inner finally
+                }
+            }
+
+            return AssertSequence(new[] { 1, 2, 3, 4, 7, 8 });
+        }
+
+        public static int AssertSequence(int[] expected)
+        {
+            if (Log.Index != expected.Length)
+                return 1;
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                if (Log.Events[i] != expected[i])
+                    return 1;
+            }
+
+            return 0;
+        }
+    }
+
+    public static class Log
+    {
+        public static int[] Events { get; } = new int[10];
+        public static int Index { get; set; }
+
+        public static void Mark(int id) => Events[Index++] = id;
+    }
+}
